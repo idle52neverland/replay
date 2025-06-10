f8b2c74f25539474d9510fb9efdbe9bb64bbeb7e
ssssffffff <idle52neverland@gmail.com>
2025-06-10 10:49:02 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,8 @@
 function normalize(str) {
   return str.replace(/\s|,|-/g, '').toLowerCase();
 }
+
+function extractDate(title) {
+  const match = title.match(/(\\d{4}-\\d{2}-\\d{2})/);
+  return match ? new Date(match[1]) : new Date(0);
+}
