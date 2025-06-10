660117b0a4c1c76d841c6201c345c7d5008c0b83
ssssffffff <idle52neverland@gmail.com>
2025-06-11 01:50:54 +0900

    Update cards-RECORDING_BEHIND.js

--- js/cards-RECORDING_BEHIND.js
+++ js/cards-RECORDING_BEHIND.js
@@ -1,5 +1,454 @@
 const recordingBehindCards = [
-  // 카드 데이터를 여기에 추가하세요
+ {
+  "link": "https://youtu.be/S3DVpMbd9Aw",
+  "category": "RECORDING_BEHIND",
+  "member": "소연",
+  "year": "2020",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/S3DVpMbd9Aw/mqdefault.jpg",
+  "alt": "소연 - HYO 효연 ‘DESSERT (Feat. Loopy, SOYEON ((G)I-DLE))’ Behind The Scenes (2020-07-21)",
+  "title": "소연 - HYO 효연 ‘DESSERT (Feat. Loopy, SOYEON ((G)I-DLE))’ Behind The Scenes (2020-07-21)"
+},
+
+{
+  "link": "https://youtu.be/ipeRGRNQiC4",
+  "category": "RECORDING_BEHIND",
+  "member": "소연, 미연",
+  "year": "2020",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/ipeRGRNQiC4/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - LoL K/DA 'THE BADDEST' 녹음 비하인드 (미연&소연) (2020-08-28)",
+  "title": "(여자)아이들((G)I-DLE) - LoL K/DA 'THE BADDEST' 녹음 비하인드 (미연&소연) (2020-08-28)"
+},
+
+{
+  "link": "https://youtu.be/v99wxx_Dkj8",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2021",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/v99wxx_Dkj8/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - 'HWAA(火/花) (Chinese Ver.)' 녹음 비하인드 (ENG) (2021-02-06)",
+  "title": "(여자)아이들((G)I-DLE) - 'HWAA(火/花) (Chinese Ver.)' 녹음 비하인드 (ENG) (2021-02-06)"
+},
+
+{
+  "link": "https://youtu.be/mgB4LrADUls",
+  "category": "RECORDING_BEHIND",
+  "member": "소연",
+  "year": "2022",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/mgB4LrADUls/mqdefault.jpg",
+  "alt": "[메이킹]불타오르게 위대하게 난리난 소연의 녹음 현장! (2022-01-17)",
+  "title": "[메이킹]불타오르게 위대하게 난리난 소연의 녹음 현장! (2022-01-17)"
+},
+
+{
+  "link": "https://youtu.be/zGTjopEZnmQ",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2022",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/zGTjopEZnmQ/mqdefault.jpg",
+  "alt": "[Blue Moon] Donghae Recording Log #3 with Miyeon from (G)-IDLE / 동해 녹음 비하인드 #3 with (여자)아이들 미연 (2022-01-29)",
+  "title": "[Blue Moon] Donghae Recording Log #3 with Miyeon from (G)-IDLE / 동해 녹음 비하인드 #3 with (여자)아이들 미연 (2022-01-29)"
+},
+
+{
+  "link": "https://youtu.be/pXVD5lLQyq0",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2022",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/pXVD5lLQyq0/mqdefault.jpg",
+  "alt": "[INTERVIEW & MAKING] 민니 ((여자)아이들) - 너의 하루를 묻고 싶어 :: 링크: 먹고 사랑하라, 죽이게 OST Part.2 (2022-06-21)",
+  "title": "[INTERVIEW & MAKING] 민니 ((여자)아이들) - 너의 하루를 묻고 싶어 :: 링크: 먹고 사랑하라, 죽이게 OST Part.2 (2022-06-21)"
+},
+
+{
+  "link": "https://youtu.be/4zKHhA_HtKQ",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2022",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/4zKHhA_HtKQ/mqdefault.jpg",
+  "alt": "헤이즈(Heize) - The 2nd Album [Undo] Album Making Film Ep.3 (2022-07-05)",
+  "title": "헤이즈(Heize) - The 2nd Album [Undo] Album Making Film Ep.3 (2022-07-05)"
+},
+
+{
+  "link": "https://youtu.be/xBdy4pG6pK4",
+  "category": "RECORDING_BEHIND",
+  "member": "소연, 미연, 민니, 우기, 슈화",
+  "year": "2022",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/xBdy4pG6pK4/mqdefault.jpg",
+  "alt": "소연 - 귀여운 사람이 멋진 척 할 때 l 서든어택 (2022-07-28)",
+  "title": "소연 - 귀여운 사람이 멋진 척 할 때 l 서든어택 (2022-07-28)"
+},
+
+{
+  "link": "https://youtu.be/F8p3RR37naI",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2022",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/F8p3RR37naI/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - 'Nxde' Recording Behind (ENG/CHN) (2022-12-23)",
+  "title": "(여자)아이들((G)I-DLE) - 'Nxde' Recording Behind (ENG/CHN) (2022-12-23)"
+},
+
+{
+  "link": "https://youtu.be/kfadkfcKUrU",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2023",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/kfadkfcKUrU/mqdefault.jpg",
+  "alt": "(여자)아이들 민니(MINNIE) 'In The Novel' 녹음현장 인터뷰 - 모든 게 착각이었다 OST (2023-01-24)",
+  "title": "(여자)아이들 민니(MINNIE) 'In The Novel' 녹음현장 인터뷰 - 모든 게 착각이었다 OST (2023-01-24)"
+},
+
+{
+  "link": "https://youtu.be/pPLX4Y37dD0",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2023",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/pPLX4Y37dD0/mqdefault.jpg",
+  "alt": "메이킹 MV | 민니 ((여자)아이들) MINNIE ((G)I-DLE) - In The Novel | 모든 게 착각이었다 OST Part 2 (2023-01-26)",
+  "title": "메이킹 MV | 민니 ((여자)아이들) MINNIE ((G)I-DLE) - In The Novel | 모든 게 착각이었다 OST Part 2 (2023-01-26)"
+},
+
+{
+  "link": "https://youtu.be/NPrqmhdrjjI",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2023",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/NPrqmhdrjjI/mqdefault.jpg",
+  "alt": "(여자)아이들 민니(MINNIE) 'In The Novel' Live Clip - 모든 게 착각이었다 OST (2023-01-26)",
+  "title": "(여자)아이들 민니(MINNIE) 'In The Novel' Live Clip - 모든 게 착각이었다 OST (2023-01-26)"
+},
+
+{
+  "link": "https://youtu.be/FaSYRHyd-2E",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2023",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/FaSYRHyd-2E/mqdefault.jpg",
+  "alt": "[에버소울] 재클린 OST 📀 「무덤덤(Numb)」 song by 미연 (2023-02-01)",
+  "title": "[에버소울] 재클린 OST 📀 「무덤덤(Numb)」 song by 미연 (2023-02-01)"
+},
+
+{
+  "link": "https://youtu.be/YlEahTLcL_Y",
+  "category": "RECORDING_BEHIND",
+  "member": "우기",
+  "year": "2023",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/YlEahTLcL_Y/mqdefault.jpg",
+  "alt": "(여자)아이들 (G)I-DLE 우기(YUQI) X 하이퍼타임(HypeerTime) 'Not Cinderella' Live Clip - 신데렐라는 내가 아니었다 OST (2023-02-23)",
+  "title": "(여자)아이들 (G)I-DLE 우기(YUQI) X 하이퍼타임(HypeerTime) 'Not Cinderella' Live Clip - 신데렐라는 내가 아니었다 OST (2023-02-23)"
+},
+
+{
+  "link": "https://youtu.be/CqBytxSQhHo",
+  "category": "RECORDING_BEHIND",
+  "member": "우기",
+  "year": "2023",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/CqBytxSQhHo/mqdefault.jpg",
+  "alt": "메이킹 MV | 우기 (YUQI (여자)아이들), 하이퍼타임 (HypeerTime) - Not Cinderella | 신데렐라는 내가 아니었다 OST Part 1 (2023-02-23)",
+  "title": "메이킹 MV | 우기 (YUQI (여자)아이들), 하이퍼타임 (HypeerTime) - Not Cinderella | 신데렐라는 내가 아니었다 OST Part 1 (2023-02-23)"
+},
+
+{
+  "link": "https://youtu.be/17I_MiCA9HQ",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2023",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/17I_MiCA9HQ/mqdefault.jpg",
+  "alt": "[Behind The Scene] 민니 ((여자)아이들) 'Expectations' 녹음 현장 (2023-03-20)",
+  "title": "[Behind The Scene] 민니 ((여자)아이들) 'Expectations' 녹음 현장 (2023-03-20)"
+},
+
+{
+  "link": "https://youtu.be/pKCaXYYwGjw",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/pKCaXYYwGjw/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - '퀸카 (Queencard)' Choreography Practice Video (2023-05-21)",
+  "title": "(여자)아이들((G)I-DLE) - '퀸카 (Queencard)' Choreography Practice Video (2023-05-21)"
+},
+
+{
+  "link": "https://youtu.be/wg7AL7inFTE",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/wg7AL7inFTE/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - '퀸카 (Queencard)' Recording Behind (ENG/CHN) (2023-05-26)",
+  "title": "(여자)아이들((G)I-DLE) - '퀸카 (Queencard)' Recording Behind (ENG/CHN) (2023-05-26)"
+},
+
+{
+  "link": "https://youtu.be/_5EOmPP9qwM",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2023",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/_5EOmPP9qwM/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - 'Paradise' & '어린 어른' Recording Behind (ENG/CHN) (2023-06-16)",
+  "title": "(여자)아이들((G)I-DLE) - 'Paradise' & '어린 어른' Recording Behind (ENG/CHN) (2023-06-16)"
+},
+
+{
+  "link": "https://youtu.be/CdSWl1tU_pk",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2023",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/CdSWl1tU_pk/mqdefault.jpg",
+  "alt": "[MV] MIYEON(미연 ((여자)아이들)) - The Painted On The Moonlight(달빛에 그려지는) (2023-08-21)",
+  "title": "[MV] MIYEON(미연 ((여자)아이들)) - The Painted On The Moonlight(달빛에 그려지는) (2023-08-21)"
+},
+
+{
+  "link": "https://youtu.be/O2sNRgiTidc",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2023",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/O2sNRgiTidc/mqdefault.jpg",
+  "alt": "[연인] OST, 미연((여자)아이들) '달빛에 그려지는' Full Ver., MBC 230819 방송 (2023-08-21)",
+  "title": "[연인] OST, 미연((여자)아이들) '달빛에 그려지는' Full Ver., MBC 230819 방송 (2023-08-21)"
+},
+
+{
+  "link": "https://youtu.be/C5VwU4rdeHU",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2023",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/C5VwU4rdeHU/mqdefault.jpg",
+  "alt": "[오늘도 사랑스럽개 OST] 민니((여자)아이들) MINNIE((G)I-DLE) '타이밍(Timing)' Special CLIP : 오늘도 사랑스럽개 OST Part. 2 (2023-11-15)",
+  "title": "[오늘도 사랑스럽개 OST] 민니((여자)아이들) MINNIE((G)I-DLE) '타이밍(Timing)' Special CLIP : 오늘도 사랑스럽개 OST Part. 2 (2023-11-15)"
+},
+
+{
+  "link": "https://youtu.be/u_rl0JqSGVY",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2024",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/u_rl0JqSGVY/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - 'Super Lady' Recording Behind (ENG/CHN) (2024-02-11)",
+  "title": "(여자)아이들((G)I-DLE) - 'Super Lady' Recording Behind (ENG/CHN) (2024-02-11)"
+},
+
+{
+  "link": "https://youtu.be/wFikxHi9WO0",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2024",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/wFikxHi9WO0/mqdefault.jpg",
+  "alt": "[연애남매] 미연 ((여자)아이들) - You and I (연애남매 OST Part.2) | Making🎙️ | My Sibling's Romance OST | GOGOSING (2024-03-22)",
+  "title": "[연애남매] 미연 ((여자)아이들) - You and I (연애남매 OST Part.2) | Making🎙️ | My Sibling's Romance OST | GOGOSING (2024-03-22)"
+},
+
+{
+  "link": "https://youtu.be/nYUetCEWXVE",
+  "category": "RECORDING_BEHIND",
+  "member": "우기",
+  "year": "2024",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/nYUetCEWXVE/mqdefault.jpg",
+  "alt": "우기(YUQI) - 'FREAK' Recording Behind (ENG/CHN) (2024-05-01)",
+  "title": "우기(YUQI) - 'FREAK' Recording Behind (ENG/CHN) (2024-05-01)"
+},
+
+{
+  "link": "https://youtu.be/n2Q4qN7kjGM",
+  "category": "RECORDING_BEHIND",
+  "member": "민니, 우기",
+  "year": "2024",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/n2Q4qN7kjGM/mqdefault.jpg",
+  "alt": "우기(YUQI) - 'Everytime (With MINNIE ((G)I-DLE))' Recording Behind (ENG/CHN) (2024-05-17)",
+  "title": "우기(YUQI) - 'Everytime (With MINNIE ((G)I-DLE))' Recording Behind (ENG/CHN) (2024-05-17)"
+},
+
+{
+  "link": "https://youtu.be/ioWv9shT4lU",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2024",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/ioWv9shT4lU/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - '클락션 (Klaxon)' Recording Behind (ENG/CHN) (2024-07-17)",
+  "title": "(여자)아이들((G)I-DLE) - '클락션 (Klaxon)' Recording Behind (ENG/CHN) (2024-07-17)"
+},
+
+{
+  "link": "https://youtu.be/dTAvYoKt6M4",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2024",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/dTAvYoKt6M4/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - 'Bloom' Recording Behind (ENG/CHN) (2024-08-02)",
+  "title": "(여자)아이들((G)I-DLE) - 'Bloom' Recording Behind (ENG/CHN) (2024-08-02)"
+},
+
+{
+  "link": "https://youtu.be/8u9879OGO14",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2024",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/8u9879OGO14/mqdefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) - 'Neverland' Recording BehindㅣSUB (2024-08-06)",
+  "title": "(여자)아이들((G)I-DLE) - 'Neverland' Recording BehindㅣSUB (2024-08-06)"
+},
+
+{
+  "link": "https://youtu.be/3Ccu3CjsPZo",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2024",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/3Ccu3CjsPZo/mqdefault.jpg",
+  "alt": "[making] 미연 ((여자)아이들) - 기도 (2024-11-13)",
+  "title": "[making] 미연 ((여자)아이들) - 기도 (2024-11-13)"
+},
+
+{
+  "link": "https://youtu.be/HKzKcnEx9Bk",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2025",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/HKzKcnEx9Bk/mqdefault.jpg",
+  "alt": "민니(MINNIE) - 'HER' Recording BehindㅣSUB (2025-02-01)",
+  "title": "민니(MINNIE) - 'HER' Recording BehindㅣSUB (2025-02-01)"
+},
+
+{
+  "link": "https://youtu.be/8V2SpdoRxTk",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2025",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/8V2SpdoRxTk/mqdefault.jpg",
+  "alt": "[MV] MIYEON ((G)I-DLE))(미연 ((여자)아이들)) _ Red Moon(적월) (2025-02-12)",
+  "title": "[MV] MIYEON ((G)I-DLE))(미연 ((여자)아이들)) _ Red Moon(적월) (2025-02-12)"
+},
+
+{
+  "link": "https://youtu.be/yvWNj1E872g",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2025",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/yvWNj1E872g/mqdefault.jpg",
+  "alt": "민니(MINNIE) - 1st Mini Album [HER] 수록곡 Recording BehindㅣSUB (2025-02-17)",
+  "title": "민니(MINNIE) - 1st Mini Album [HER] 수록곡 Recording BehindㅣSUB (2025-02-17)"
+},
+
+{
+  "link": "https://youtu.be/OAISVzre8QQ",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2025",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/OAISVzre8QQ/mqdefault.jpg",
+  "alt": "[MV] MINNIE(민니) ((G)I-DLE ((여자)아이들)) - Draw the Moon(달이 그려지다) (feat. MIYAVI) (2025-03-21)",
+  "title": "[MV] MINNIE(민니) ((G)I-DLE ((여자)아이들)) - Draw the Moon(달이 그려지다) (feat. MIYAVI) (2025-03-21)"
+},
+
+{
+  "link": "https://youtu.be/pdUV1R2ZObs",
+  "category": "RECORDING_BEHIND",
+  "member": "소연, 미연, 민니, 우기, 슈화",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/pdUV1R2ZObs/mqdefault.jpg",
+  "alt": "소연 - [나 혼자만 레벨업:ARISE] (여자)아이들 콜라보 기념 영상 편지 (2025-04-12)",
+  "title": "소연 - [나 혼자만 레벨업:ARISE] (여자)아이들 콜라보 기념 영상 편지 (2025-04-12)"
+},
+
+{
+  "link": "https://youtu.be/HxROxHdBQ2g",
+  "category": "RECORDING_BEHIND",
+  "member": "미연",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/HxROxHdBQ2g/mqdefault.jpg",
+  "alt": "미연(MIYEON), Jike Junyi - 'Glow Up' SPECIAL CLIP (2025-04-17)",
+  "title": "미연(MIYEON), Jike Junyi - 'Glow Up' SPECIAL CLIP (2025-04-17)"
+},
+
+{
+  "link": "https://youtu.be/i2wG04YoEOg",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/i2wG04YoEOg/mqdefault.jpg",
+  "alt": "[언젠가는 슬기로울 전공의생활 (Resident Playbook) OST Part 5] 민니 (MINNIE) - 숨 (Breath) MV Teaser (2025-04-24)",
+  "title": "[언젠가는 슬기로울 전공의생활 (Resident Playbook) OST Part 5] 민니 (MINNIE) - 숨 (Breath) MV Teaser (2025-04-24)"
+},
+
+{
+  "link": "https://youtu.be/eOFfOLZOp0M",
+  "category": "RECORDING_BEHIND",
+  "member": "민니",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/eOFfOLZOp0M/mqdefault.jpg",
+  "alt": "[MaumC Record] 민니 ((여자)아이들) | 숨 | Recording Behind | 녹음 비하인드 (2025-04-26)",
+  "title": "[MaumC Record] 민니 ((여자)아이들) | 숨 | Recording Behind | 녹음 비하인드 (2025-04-26)"
+},
+
+{
+  "link": "https://youtu.be/Ly5SxYH5OhI",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/Ly5SxYH5OhI/mqdefault.jpg",
+  "alt": "i-dle (아이들) 'Good Thing' Recording BehindㅣSUB (2025-05-27)",
+  "title": "i-dle (아이들) 'Good Thing' Recording BehindㅣSUB (2025-05-27)"
+},
+
+{
+  "link": "https://youtu.be/Rf0ajz4PrEg",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2025",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/Rf0ajz4PrEg/mqdefault.jpg",
+  "alt": "i-dle (아이들) 'Girlfriend' Recording BehindㅣSUB (2025-06-03)",
+  "title": "i-dle (아이들) 'Girlfriend' Recording BehindㅣSUB (2025-06-03)"
+},
+
+{
+  "link": "https://youtu.be/u3Yu5N3CMNw",
+  "category": "RECORDING_BEHIND",
+  "member": "아이들",
+  "year": "2025",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/u3Yu5N3CMNw/mqdefault.jpg",
+  "alt": "i-dle (아이들) 'Love Tease' Recording BehindㅣSUB (2025-06-10)",
+  "title": "i-dle (아이들) 'Love Tease' Recording BehindㅣSUB (2025-06-10)"
+}
 
 
 
