bbab4e93a1ab228e88fb239544eaf1f58368c02a
ssssffffff <idle52neverland@gmail.com>
2025-06-11 18:09:01 +0900

    Update cards-RADIO_PODCAST.js

--- js/cards-RADIO_PODCAST.js
+++ js/cards-RADIO_PODCAST.js
@@ -1,8 +1,487 @@
 const radioPodcastCards = [
-  // 카드 데이터를 여기에 추가하세요
+  {
+  "link": "https://youtu.be/AsjBiO1gsJs",
+  "category": "RADIO_PODCAST",
+  "member": "미연, 민니",
+  "year": "2025",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/AsjBiO1gsJs/sddefault.jpg",
+  "alt": "[FULL] 다시 시작! 2막을 시작하다!!💞 완전체로 돌아온 아이들(i-dle)의 미연, 민니 보는 라디오 | 박소현의 러브게임 | 250601 (2025-06-01)",
+  "title": "[FULL] 다시 시작! 2막을 시작하다!!💞 완전체로 돌아온 아이들(i-dle)의 미연, 민니 보는 라디오 | 박소현의 러브게임 | 250601 (2025-06-01)"
+},
 
+{
+  "link": "https://youtu.be/inaLcT_gOS8",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/inaLcT_gOS8/sddefault.jpg",
+  "alt": "[FULL] 함께해서 두 배로 즐거웠던 시간🥳 아이들(idle), 조째즈(ZO ZAZZ) 보는 라디오 | 두시탈출 컬투쇼 | 250527 (2025-05-27)",
+  "title": "[FULL] 함께해서 두 배로 즐거웠던 시간🥳 아이들(idle), 조째즈(ZO ZAZZ) 보는 라디오 | 두시탈출 컬투쇼 | 250527 (2025-05-27)"
+},
 
+{
+  "link": "https://youtu.be/ISsi7sLXI8g",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/ISsi7sLXI8g/sddefault.jpg",
+  "alt": "[이은지의 가요광장]  with 아이들  | KBS 250520(화) 방송 (2025-05-20)",
+  "title": "[이은지의 가요광장]  with 아이들  | KBS 250520(화) 방송 (2025-05-20)"
+},
 
+{
+  "link": "https://youtu.be/QieM1oxUFUI",
+  "category": "RADIO_PODCAST",
+  "member": "미연",
+  "year": "2025",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/QieM1oxUFUI/sddefault.jpg",
+  "alt": "가광 초대석, 누구세요? with 브브걸 (BBGIRLS) (스페셜 DJ 미연) full ver. / [이은지의 가요광장] I KBS 250219 방송 (2025-02-19)",
+  "title": "가광 초대석, 누구세요? with 브브걸 (BBGIRLS) (스페셜 DJ 미연) full ver. / [이은지의 가요광장] I KBS 250219 방송 (2025-02-19)"
+},
+
+{
+  "link": "https://youtu.be/jsr9XqQh-C8",
+  "category": "RADIO_PODCAST",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/jsr9XqQh-C8/sddefault.jpg",
+  "alt": "[FULL] Cause I’m HER!!💗 다른 누구도 아닌 솔로가수 민니(MINNIE) 보는 라디오 | 웬디의 영스트리트 | 250127 (2025-01-28)",
+  "title": "[FULL] Cause I’m HER!!💗 다른 누구도 아닌 솔로가수 민니(MINNIE) 보는 라디오 | 웬디의 영스트리트 | 250127 (2025-01-28)"
+},
+
+{
+  "link": "https://youtu.be/AKbHvJDWLC8",
+  "category": "RADIO_PODCAST",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/AKbHvJDWLC8/sddefault.jpg",
+  "alt": "이은지의 가요광장 with 민니((여자)아이들) KBS 250130(목) 방송 (2025-01-30)",
+  "title": "이은지의 가요광장 with 민니((여자)아이들) KBS 250130(목) 방송 (2025-01-30)"
+},
+
+{
+  "link": "https://youtu.be/5rLPgvdUnJ4",
+  "category": "RADIO_PODCAST",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/5rLPgvdUnJ4/sddefault.jpg",
+  "alt": "[FULL] who shot ya?🔫 내 심장을 저격한 민니(MINNIE) 보는 라디오 | 두시탈출 컬투쇼 | 250126 (2025-01-26)",
+  "title": "[FULL] who shot ya?🔫 내 심장을 저격한 민니(MINNIE) 보는 라디오 | 두시탈출 컬투쇼 | 250126 (2025-01-26)"
+},
+
+{
+  "link": "https://youtu.be/OorZpjSm7QE",
+  "category": "RADIO_PODCAST",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/OorZpjSm7QE/sddefault.jpg",
+  "alt": "볼륨 초대석 : 최효정의 비밀정원 with 민니 (MINNIE) full ver. / [오마이걸 효정의 볼륨을 높여요] | KBS 250123 방송 (2025-01-23)",
+  "title": "볼륨 초대석 : 최효정의 비밀정원 with 민니 (MINNIE) full ver. / [오마이걸 효정의 볼륨을 높여요] | KBS 250123 방송 (2025-01-23)"
+},
+
+{
+  "link": "https://youtu.be/x8BwPfJdncw",
+  "category": "RADIO_PODCAST",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/x8BwPfJdncw/sddefault.jpg",
+  "alt": "[FULL] 솔로 데뷔 2일차 ✨민니✨ 앨범 잉크도 마르기 전에 '헐'레벌떡 라이브 온 에어로 모셨습니다💗 | 정오의 희망곡 김신영입니다 | MBC 250122 방송 (2025-01-22)",
+  "title": "[FULL] 솔로 데뷔 2일차 ✨민니✨ 앨범 잉크도 마르기 전에 '헐'레벌떡 라이브 온 에어로 모셨습니다💗 | 정오의 희망곡 김신영입니다 | MBC 250122 방송 (2025-01-22)"
+},
+
+{
+  "link": "https://youtu.be/HFYobCvufa8",
+  "category": "RADIO_PODCAST",
+  "member": "소연",
+  "year": "2024",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/HFYobCvufa8/sddefault.jpg",
+  "alt": "[FULL] 정희 대장님 ✨신디✨와 아이들 선장님 ✨소연✨ 조합❤️ 소리 대신 클락션 질러❗️| 정오의 희망곡 김신영입니다 | MBC 240722 방송 (2024-07-22)",
+  "title": "[FULL] 정희 대장님 ✨신디✨와 아이들 선장님 ✨소연✨ 조합❤️ 소리 대신 클락션 질러❗️| 정오의 희망곡 김신영입니다 | MBC 240722 방송 (2024-07-22)"
+},
+
+{
+  "link": "https://youtu.be/Xsu0iUK_W8A",
+  "category": "RADIO_PODCAST",
+  "member": "미연, 우기",
+  "year": "2024",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/Xsu0iUK_W8A/sddefault.jpg",
+  "alt": "[Full ver.] 미연&우기가 밤라에 나온다!!!! 쪼꼬들 클락션 울려!!!🛞🚘✨ \"나잇투밋츄\" w/(여자)아이들 우기&미연 (2024-07-18)",
+  "title": "[Full ver.] 미연&우기가 밤라에 나온다!!!! 쪼꼬들 클락션 울려!!!🛞🚘✨ \"나잇투밋츄\" w/(여자)아이들 우기&미연 (2024-07-18)"
+},
+
+{
+  "link": "https://youtu.be/XhO80sPC0u8",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2024",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/XhO80sPC0u8/sddefault.jpg",
+  "alt": "이은지의 가요광장 with (여자)아이들 ((G)I-DLE) | KBS 240709(화) 방송 (2024-07-09)",
+  "title": "이은지의 가요광장 with (여자)아이들 ((G)I-DLE) | KBS 240709(화) 방송 (2024-07-09)"
+},
+
+{
+  "link": "https://youtu.be/TLO_rkq7gBc",
+  "category": "RADIO_PODCAST",
+  "member": "소연",
+  "year": "2024",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/TLO_rkq7gBc/sddefault.jpg",
+  "alt": "[FULL] ✨전소연✨ 암더탑 Super Lady로 정희 접수완❤️｜정오의 희망곡 김신영입니다｜MBC 240214 방송 (2024-02-14)",
+  "title": "[FULL] ✨전소연✨ 암더탑 Super Lady로 정희 접수완❤️｜정오의 희망곡 김신영입니다｜MBC 240214 방송 (2024-02-14)"
+},
+
+{
+  "link": "https://youtu.be/Z7akYetUqcI",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2023",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/Z7akYetUqcI/sddefault.jpg",
+  "alt": "(G)I-DLE Reveals Who They Would Like To Collaborate With (2023-08-09)",
+  "title": "(G)I-DLE Reveals Who They Would Like To Collaborate With (2023-08-09)"
+},
+
+{
+  "link": "https://youtu.be/PryFedOdq9s",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2023",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/PryFedOdq9s/sddefault.jpg",
+  "alt": "(G)I-DLE Reveals Their Favorite Part About NYC (2023-08-09)",
+  "title": "(G)I-DLE Reveals Their Favorite Part About NYC (2023-08-09)"
+},
+
+{
+  "link": "https://youtu.be/jhJSaQxl1zY",
+  "category": "RADIO_PODCAST",
+  "member": "민니, 우기",
+  "year": "2023",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/jhJSaQxl1zY/sddefault.jpg",
+  "alt": "[Full ver.] 디스했다가 훈훈했다가 여기 완전 💥찐남매 케미 맛집💥 l 나잇투밋츄 w. (여자)아이들 G)I-DLE 민니&우기 (2023-06-21)",
+  "title": "[Full ver.] 디스했다가 훈훈했다가 여기 완전 💥찐남매 케미 맛집💥 l 나잇투밋츄 w. (여자)아이들 G)I-DLE 민니&우기 (2023-06-21)"
+},
+
+{
+  "link": "https://youtu.be/IU7MAHrW7Hg",
+  "category": "RADIO_PODCAST",
+  "member": "소연, 미연, 우기",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/IU7MAHrW7Hg/sddefault.jpg",
+  "alt": "[FULL] 그냥 예쁘게 태어.난.곤.데💕👑퀸카 (여자)아이들((G)I-DLE)👑 미연, 우기, 소연 보는 라디오 | 두시탈출 컬투쇼 (2023-05-31)",
+  "title": "[FULL] 그냥 예쁘게 태어.난.곤.데💕👑퀸카 (여자)아이들((G)I-DLE)👑 미연, 우기, 소연 보는 라디오 | 두시탈출 컬투쇼 (2023-05-31)"
+},
+
+{
+  "link": "https://youtu.be/bRsmqbo5UOc",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/bRsmqbo5UOc/sddefault.jpg",
+  "alt": "전설의 고수 with (여자)아이들 full ver. /[박명수의 라디오쇼]｜KBS 230522 방송 (2023-05-22)",
+  "title": "전설의 고수 with (여자)아이들 full ver. /[박명수의 라디오쇼]｜KBS 230522 방송 (2023-05-22)"
+},
+
+{
+  "link": "https://youtu.be/v2Sb7KtK_tY",
+  "category": "RADIO_PODCAST",
+  "member": "소연, 미연, 민니",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/v2Sb7KtK_tY/sddefault.jpg",
+  "alt": "이은지의 가요광장 with 아이들((G)I-DLE) 미연,민니,소연 230517 (2023-05-17)",
+  "title": "이은지의 가요광장 with 아이들((G)I-DLE) 미연,민니,소연 230517 (2023-05-17)"
+},
+
+{
+  "link": "https://youtu.be/h4kcfz8Uea4",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/h4kcfz8Uea4/sddefault.jpg",
+  "alt": "원샷초대석 with (여자)아이들 full ver. / [비투비의 키스 더 라디오] | KBS 230516 방송 (2023-05-17)",
+  "title": "원샷초대석 with (여자)아이들 full ver. / [비투비의 키스 더 라디오] | KBS 230516 방송 (2023-05-17)"
+},
+
+{
+  "link": "https://youtu.be/pg0wx6nAGdQ",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2022",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/pg0wx6nAGdQ/sddefault.jpg",
+  "alt": "전설의 고수 with (여자)아이들 full ver. /[박명수의 라디오쇼]｜KBS 221114 방송 (2022-11-14)",
+  "title": "전설의 고수 with (여자)아이들 full ver. /[박명수의 라디오쇼]｜KBS 221114 방송 (2022-11-14)"
+},
+
+{
+  "link": "https://youtu.be/S0YWmCh3CvM",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2022",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/S0YWmCh3CvM/sddefault.jpg",
+  "alt": "[FULL][4K] (여자)아이들((G)I-DLE) | 박소현의 러브게임 | 금주의 인기가요 톱텐 | The K-pop Stars Radio (2022-11-12)",
+  "title": "[FULL][4K] (여자)아이들((G)I-DLE) | 박소현의 러브게임 | 금주의 인기가요 톱텐 | The K-pop Stars Radio (2022-11-12)"
+},
+
+{
+  "link": "https://youtu.be/7uAq3vuboEI",
+  "category": "RADIO_PODCAST",
+  "member": "미연, 민니, 슈화",
+  "year": "2022",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/7uAq3vuboEI/sddefault.jpg",
+  "alt": "기광 막힌 초대석 with (여자)아이들 (미연, 민니, 슈화) full ver./ [이기광의 가요광장] I KBS 221025 방송 (2022-10-25)",
+  "title": "기광 막힌 초대석 with (여자)아이들 (미연, 민니, 슈화) full ver./ [이기광의 가요광장] I KBS 221025 방송 (2022-10-25)"
+},
+
+{
+  "link": "https://youtu.be/AgXZkCieP50",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2022",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/AgXZkCieP50/sddefault.jpg",
+  "alt": "볼륨 초대석 : 왔어요 with (여자)아이들 ((G)I-DLE) full ver./ [헤이즈의 볼륨을 높여요] I KBS 221024 방송 (2022-10-24)",
+  "title": "볼륨 초대석 : 왔어요 with (여자)아이들 ((G)I-DLE) full ver./ [헤이즈의 볼륨을 높여요] I KBS 221024 방송 (2022-10-24)"
+},
+
+{
+  "link": "https://youtu.be/sRguxMpZMB0",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2022",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/sRguxMpZMB0/sddefault.jpg",
+  "alt": "비투비의 키스더라디오(BTOB Kiss the radio) with (G)I-DLE (여자)아이들 221020 (2022-10-20)",
+  "title": "비투비의 키스더라디오(BTOB Kiss the radio) with (G)I-DLE (여자)아이들 221020 (2022-10-20)"
+},
+
+{
+  "link": "https://youtu.be/Avq13ocXYz8",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2022",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/Avq13ocXYz8/sddefault.jpg",
+  "alt": "[FULL] 가장 (여자)아이들다운 모습을 입고 따따랏따라~ / GOT7 영재의 친한친구 / MBC 221020 방송 (2022-10-20)",
+  "title": "[FULL] 가장 (여자)아이들다운 모습을 입고 따따랏따라~ / GOT7 영재의 친한친구 / MBC 221020 방송 (2022-10-20)"
+},
+
+{
+  "link": "https://youtu.be/ojYeaY5D8lk",
+  "category": "RADIO_PODCAST",
+  "member": "미연",
+  "year": "2022",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/ojYeaY5D8lk/sddefault.jpg",
+  "alt": "[FULL] 비슷한 부분까지 닮은 최애의 조합 탄생💖 미연(MIYEON) 보는라디오 | 웬디의 영스트리트 | 220502 (2022-05-03)",
+  "title": "[FULL] 비슷한 부분까지 닮은 최애의 조합 탄생💖 미연(MIYEON) 보는라디오 | 웬디의 영스트리트 | 220502 (2022-05-03)"
+},
+
+{
+  "link": "https://youtu.be/rylW3vpKSfk",
+  "category": "RADIO_PODCAST",
+  "member": "미연",
+  "year": "2022",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/rylW3vpKSfk/sddefault.jpg",
+  "alt": "[FULL] 존재 자체가 프린세스❤️ 미연(MIYEON) | (여자)아이들((G)I-DLE) | 두시탈출 컬투쇼 | 220501 (2022-05-01)",
+  "title": "[FULL] 존재 자체가 프린세스❤️ 미연(MIYEON) | (여자)아이들((G)I-DLE) | 두시탈출 컬투쇼 | 220501 (2022-05-01)"
+},
+
+{
+  "link": "https://youtu.be/loGopFmdBD0",
+  "category": "RADIO_PODCAST",
+  "member": "미연",
+  "year": "2022",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/loGopFmdBD0/sddefault.jpg",
+  "alt": "비투비의 키스더라디오(BTOB Kiss the radio) with 미연(아이들)(MIYEON((G)I-DLE)) 220427 (2022-04-27)",
+  "title": "비투비의 키스더라디오(BTOB Kiss the radio) with 미연(아이들)(MIYEON((G)I-DLE)) 220427 (2022-04-27)"
+},
+
+{
+  "link": "https://youtu.be/qdYeb4z_F-Y",
+  "category": "RADIO_PODCAST",
+  "member": "민니, 우기",
+  "year": "2022",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/qdYeb4z_F-Y/sddefault.jpg",
+  "alt": "[Full ver.] 펜타곤 신원이 아이들 민니&우기한테 소리지른 사연ㅋㅋㅋ l 나잇투밋츄 w.(여자)아이들 민니&우기 (2022-04-18)",
+  "title": "[Full ver.] 펜타곤 신원이 아이들 민니&우기한테 소리지른 사연ㅋㅋㅋ l 나잇투밋츄 w.(여자)아이들 민니&우기 (2022-04-18)"
+},
+
+{
+  "link": "https://youtu.be/RwKaWHCHOkc",
+  "category": "RADIO_PODCAST",
+  "member": "미연, 민니, 우기",
+  "year": "2022",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/RwKaWHCHOkc/sddefault.jpg",
+  "alt": "[FULL][4K] (여자)아이들((G)I-DLE) 미연, 우기, 민니  | 박소현의 러브게임 | 금주의 인기가요 톱텐 | The K-pop Stars Radio (2022-03-26)",
+  "title": "[FULL][4K] (여자)아이들((G)I-DLE) 미연, 우기, 민니  | 박소현의 러브게임 | 금주의 인기가요 톱텐 | The K-pop Stars Radio (2022-03-26)"
+},
+
+{
+  "link": "https://youtu.be/R1PJt5KQak8",
+  "category": "RADIO_PODCAST",
+  "member": "소연",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/R1PJt5KQak8/sddefault.jpg",
+  "alt": "라디오 토토 with 소연(of (여자)아이들) full ver. /[정은지의 가요광장] l KBS 210721 방송 (2021-07-21)",
+  "title": "라디오 토토 with 소연(of (여자)아이들) full ver. /[정은지의 가요광장] l KBS 210721 방송 (2021-07-21)"
+},
+
+{
+  "link": "https://youtu.be/IGr-H7l8jcQ",
+  "category": "RADIO_PODCAST",
+  "member": "소연",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/IGr-H7l8jcQ/sddefault.jpg",
+  "alt": "본인등판! 전소연 full ver. /[Day6의 키스 더 라디오] l kbs 210711방송 (2021-07-11)",
+  "title": "본인등판! 전소연 full ver. /[Day6의 키스 더 라디오] l kbs 210711방송 (2021-07-11)"
+},
+
+{
+  "link": "https://youtu.be/3tVxtSw1Juk",
+  "category": "RADIO_PODCAST",
+  "member": "미연, 민니",
+  "year": "2021",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/3tVxtSw1Juk/sddefault.jpg",
+  "alt": "[FULL][4K] (여자)아이들((G)I-DLE) 민니, 미연 | 박소현의 러브게임 | 금주의 인기가요 톱텐 | The K-pop Stars Radio (2021-02-06)",
+  "title": "[FULL][4K] (여자)아이들((G)I-DLE) 민니, 미연 | 박소현의 러브게임 | 금주의 인기가요 톱텐 | The K-pop Stars Radio (2021-02-06)"
+},
+
+{
+  "link": "https://youtu.be/jwWoos-T5aE",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2021",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/jwWoos-T5aE/sddefault.jpg",
+  "alt": "[FULL] 1위하고 달려온 (여자)아이들((G)I-DLE))🎉 보는 라디오 | 이준의 영스트리트 | 210122 (광고, 음악삭제) (2021-01-25)",
+  "title": "[FULL] 1위하고 달려온 (여자)아이들((G)I-DLE))🎉 보는 라디오 | 이준의 영스트리트 | 210122 (광고, 음악삭제) (2021-01-25)"
+},
+
+{
+  "link": "https://youtu.be/eFe6ocZWY5E",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2021",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/eFe6ocZWY5E/sddefault.jpg",
+  "alt": "본인등판 with  (여자)아이들((G)I-DLE) full ver. /[Day6의 키스 더 라디오] l kbs 210113 방송 (2021-01-14)",
+  "title": "본인등판 with  (여자)아이들((G)I-DLE) full ver. /[Day6의 키스 더 라디오] l kbs 210113 방송 (2021-01-14)"
+},
+
+{
+  "link": "https://youtu.be/zEmJahH-WRw",
+  "category": "RADIO_PODCAST",
+  "member": "소연",
+  "year": "2020",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/zEmJahH-WRw/sddefault.jpg",
+  "alt": "[박소현의 러브게임] 🌟에라오 단독공개🌟(여자)아이들((G)I-DLE) 수진(Soojin), 소연(Soyeon) | K-pop special (2020-04-25)",
+  "title": "[박소현의 러브게임] 🌟에라오 단독공개🌟(여자)아이들((G)I-DLE) 수진(Soojin), 소연(Soyeon) | K-pop special (2020-04-25)"
+},
+
+{
+  "link": "https://youtu.be/_n7d83zyV4Y",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2020",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/_n7d83zyV4Y/sddefault.jpg",
+  "alt": "[최파타]🎊 Oh my god 첫 그랜드슬램 🎊달성한(여자)아이들((G)I-DLE) | 보는 라디오 Full ver.│20200421 (광고, 음악삭제) (2020-04-21)",
+  "title": "[최파타]🎊 Oh my god 첫 그랜드슬램 🎊달성한(여자)아이들((G)I-DLE) | 보는 라디오 Full ver.│20200421 (광고, 음악삭제) (2020-04-21)"
+},
+
+{
+  "link": "https://youtu.be/rclhcbOIcHc",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2020",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/rclhcbOIcHc/sddefault.jpg",
+  "alt": "키스더 초대석 with (여자)아이들  full ver. /200414[박원의 키스 더 라디오] (2020-04-15)",
+  "title": "키스더 초대석 with (여자)아이들  full ver. /200414[박원의 키스 더 라디오] (2020-04-15)"
+},
+
+{
+  "link": "https://youtu.be/4-hj0AyLz3k",
+  "category": "RADIO_PODCAST",
+  "member": "미연, 우기",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/4-hj0AyLz3k/sddefault.jpg",
+  "alt": "[FULL] 모든 G는 처단한다❌ 오롯이 'idle'이라는 이름으로 빛날 분금치✨아이들 미연&우기✨와 함께하는 브카☕️💜 | 이석훈의 브런치 카페 | MBC 250521 방송 (2025-05-21)",
+  "title": "[FULL] 모든 G는 처단한다❌ 오롯이 'idle'이라는 이름으로 빛날 분금치✨아이들 미연&우기✨와 함께하는 브카☕️💜 | 이석훈의 브런치 카페 | MBC 250521 방송 (2025-05-21)"
+},
+
+{
+  "link": "https://youtu.be/gUSubxDc5vE",
+  "category": "RADIO_PODCAST",
+  "member": "민니, 우기, 슈화",
+  "year": "2022",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/gUSubxDc5vE/sddefault.jpg",
+  "alt": "[FULL] 🤟‘아이들이 아이들했다’🤟 (여자)아이들((G)I-DLE) 민니, 우기, 슈화 | 추적 30분 | 두시탈출 컬투쇼 | 220320 (2022-03-20)",
+  "title": "[FULL] 🤟‘아이들이 아이들했다’🤟 (여자)아이들((G)I-DLE) 민니, 우기, 슈화 | 추적 30분 | 두시탈출 컬투쇼 | 220320 (2022-03-20)"
+},
+
+{
+  "link": "https://youtu.be/chMiGr_W8Zo",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2022",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/chMiGr_W8Zo/sddefault.jpg",
+  "alt": "원샷 초대석 with (여자)아이들((G)I-DLE) full ver. / [비투비의 키스 더 라디오] l KBS 220318 방송 (2022-03-18)",
+  "title": "원샷 초대석 with (여자)아이들((G)I-DLE) full ver. / [비투비의 키스 더 라디오] l KBS 220318 방송 (2022-03-18)"
+},
+
+{
+  "link": "https://youtu.be/-nuwY7zCK-k",
+  "category": "RADIO_PODCAST",
+  "member": "아이들",
+  "year": "2020",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/-nuwY7zCK-k/sddefault.jpg",
+  "alt": "키스 더 음감회 with (여자)아이들((G)I-DLE) full ver. /200821[박원의 키스 더 라디오] (2020-08-22)",
+  "title": "키스 더 음감회 with (여자)아이들((G)I-DLE) full ver. /200821[박원의 키스 더 라디오] (2020-08-22)"
+},
+
+{
+  "link": "https://youtu.be/7l2FpaYWP4Q",
+  "category": "RADIO_PODCAST",
+  "member": "소연, 우기",
+  "year": "2020",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/7l2FpaYWP4Q/sddefault.jpg",
+  "alt": "[박소현의 러브게임][4K]🌟에라오 단독공개🌟 금주의 인기가요 톱텐 | (여자)아이들((G)I-DLE) 소연,우기 | The K-pop Stars Radio (2020-08-15)",
+  "title": "[박소현의 러브게임][4K]🌟에라오 단독공개🌟 금주의 인기가요 톱텐 | (여자)아이들((G)I-DLE) 소연,우기 | The K-pop Stars Radio (2020-08-15)"
+}
 
 
   
