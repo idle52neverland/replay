be260223b8d3cf69f4ba54b37f8b3aa123861571
ssssffffff <idle52neverland@gmail.com>
2025-06-12 04:18:45 +0900

    Update cards-ETC.js

--- js/cards-ETC.js
+++ js/cards-ETC.js
@@ -1,5 +1,618 @@
 const etcCards = [
-  // 카드 데이터를 여기에 추가하세요
+{
+  "link": "https://youtu.be/tv1LLHJ8u3A",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2024",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/tv1LLHJ8u3A/sddefault.jpg",
+  "alt": "대한민국 행복충전! 카드형 온누리상품권 CF (20') (2024-07-31)",
+  "title": "대한민국 행복충전! 카드형 온누리상품권 CF (20') (2024-07-31)"
+},
+
+{
+  "link": "https://youtu.be/_aiduDiq0Bc",
+  "category": "SHORTS",
+  "member": "우기",
+  "year": "2023",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/_aiduDiq0Bc/sddefault.jpg",
+  "alt": "[세리박스] (여자)아이들 '우기'가 매일 1잔씩 마시는 물은?ㅣ세리번 비우밍 다이어트 (2023-10-27)",
+  "title": "[세리박스] (여자)아이들 '우기'가 매일 1잔씩 마시는 물은?ㅣ세리번 비우밍 다이어트 (2023-10-27)"
+},
+
+{
+  "link": "https://youtu.be/TEcxkLaCHq0",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2019",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/TEcxkLaCHq0/sddefault.jpg",
+  "alt": "[ENG SUB] (여자)아이들은 세뇨리타, 악플러는 불타! #G_I_DLE│댓변인들│AYO 에이요 (2019-03-08)",
+  "title": "[ENG SUB] (여자)아이들은 세뇨리타, 악플러는 불타! #G_I_DLE│댓변인들│AYO 에이요 (2019-03-08)"
+},
+
+{
+  "link": "https://youtu.be/KyrC8lVbKSg",
+  "category": "SHORTS",
+  "member": "우기",
+  "year": "2023",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/KyrC8lVbKSg/sddefault.jpg",
+  "alt": "[세리박스] (여자)아이들 '우기'가 몰래 알려주는 비밀은?ㅣ세리번 나이트 (2023-10-24)",
+  "title": "[세리박스] (여자)아이들 '우기'가 몰래 알려주는 비밀은?ㅣ세리번 나이트 (2023-10-24)"
+},
+
+{
+  "link": "https://youtu.be/ZzZk-iOnJ48",
+  "category": "SHORTS",
+  "member": "민니",
+  "year": "2022",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/ZzZk-iOnJ48/sddefault.jpg",
+  "alt": "말해모해💓 예쁨 그 잡채(ღ•͈ᴗ•͈ღ) 예삐예삐 민니(MINNIE)의 올겨울 태국갈때 여기어때🌎🎵 (2022-11-16)",
+  "title": "말해모해💓 예쁨 그 잡채(ღ•͈ᴗ•͈ღ) 예삐예삐 민니(MINNIE)의 올겨울 태국갈때 여기어때🌎🎵 (2022-11-16)"
+},
+
+{
+  "link": "https://youtu.be/6V7sCMVX3z8",
+  "category": "SHORTS",
+  "member": "민니",
+  "year": "2022",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/6V7sCMVX3z8/sddefault.jpg",
+  "alt": "올겨울 해외갈때 여기어때🌎🎵 (ft.장기하, 민니, 이용진, 마츠다, 그렉, 미초바, 파트리샤, 파비앙) (2022-11-10)",
+  "title": "올겨울 해외갈때 여기어때🌎🎵 (ft.장기하, 민니, 이용진, 마츠다, 그렉, 미초바, 파트리샤, 파비앙) (2022-11-10)"
+},
+
+{
+  "link": "https://youtu.be/-pMAHu9i180",
+  "category": "SHORTS",
+  "member": "소연",
+  "year": "2021",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/-pMAHu9i180/sddefault.jpg",
+  "alt": "세상의 기준을 불태워라! 불닭볶음면(Main casting \"(여자) 아이들 소연\") (2021-12-17)",
+  "title": "세상의 기준을 불태워라! 불닭볶음면(Main casting \"(여자) 아이들 소연\") (2021-12-17)"
+},
+
+{
+  "link": "https://youtu.be/0IKnV6vm56U",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2023",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/0IKnV6vm56U/sddefault.jpg",
+  "alt": "(여자)아이들((G)I-DLE) X Buldak - 플레이 불닭(Play Buldak) Official Music Video (2023-07-27)",
+  "title": "(여자)아이들((G)I-DLE) X Buldak - 플레이 불닭(Play Buldak) Official Music Video (2023-07-27)"
+},
+
+{
+  "link": "https://youtu.be/yjdC6pC9Y_A",
+  "category": "SHORTS",
+  "member": "미연",
+  "year": "2022",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/yjdC6pC9Y_A/sddefault.jpg",
+  "alt": "[비원츠] 새로운 뮤즈, (여자)아이들 미연 인터뷰 & 화보 촬영 현장 공개 ✨ (2022-10-19)",
+  "title": "[비원츠] 새로운 뮤즈, (여자)아이들 미연 인터뷰 & 화보 촬영 현장 공개 ✨ (2022-10-19)"
+},
+
+{
+  "link": "https://youtu.be/i834jc0BEeY",
+  "category": "SHORTS",
+  "member": "민니",
+  "year": "2022",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/i834jc0BEeY/sddefault.jpg",
+  "alt": "[M·A·C X 민니] 스튜디오 픽스 파운데이션 & 쿠션 (2022-09-02)",
+  "title": "[M·A·C X 민니] 스튜디오 픽스 파운데이션 & 쿠션 (2022-09-02)"
+},
+
+{
+  "link": "https://youtu.be/bxN1CNjok7k",
+  "category": "SHORTS",
+  "member": "소연, 미연, 민니",
+  "year": "2022",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/bxN1CNjok7k/sddefault.jpg",
+  "alt": "WHO'S THE REAL REAL? / (여자)아이들 미연, 소연, 민니가 쏘아 올린 무한한 파장, 아디다스X구찌 스펙트럼의 존재 이유. (2022-06-12)",
+  "title": "WHO'S THE REAL REAL? / (여자)아이들 미연, 소연, 민니가 쏘아 올린 무한한 파장, 아디다스X구찌 스펙트럼의 존재 이유. (2022-06-12)"
+},
+
+{
+  "link": "https://youtu.be/kz5-_M3dMqA",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/kz5-_M3dMqA/sddefault.jpg",
+  "alt": "[메이킹필름] 20FW 아키클래식 X (여자)아이들 '컬러로 취향저격' (2020-11-12)",
+  "title": "[메이킹필름] 20FW 아키클래식 X (여자)아이들 '컬러로 취향저격' (2020-11-12)"
+},
+
+{
+  "link": "https://youtu.be/IujeJa_oHf8",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/IujeJa_oHf8/sddefault.jpg",
+  "alt": "[K-Community Challenge] Promotional video with (G)I-DLE (2020-09-30)",
+  "title": "[K-Community Challenge] Promotional video with (G)I-DLE (2020-09-30)"
+},
+
+{
+  "link": "https://youtu.be/Ay09jyHBjRA",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/Ay09jyHBjRA/sddefault.jpg",
+  "alt": "[K-Community Challenge] Ganggangsullae with (G)I-DLE (2020-09-29)",
+  "title": "[K-Community Challenge] Ganggangsullae with (G)I-DLE (2020-09-29)"
+},
+
+{
+  "link": "https://youtu.be/i5T_CLGdA7U",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/i5T_CLGdA7U/sddefault.jpg",
+  "alt": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 SUMMER LOOKBOOK BRAND FILM FULL.ver (2020-06-03)",
+  "title": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 SUMMER LOOKBOOK BRAND FILM FULL.ver (2020-06-03)"
+},
+
+{
+  "link": "https://youtu.be/OO302DhvEBk",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/OO302DhvEBk/sddefault.jpg",
+  "alt": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 S/S LOOKBOOK MAKING FILM ver.2 (2020-03-05)",
+  "title": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 S/S LOOKBOOK MAKING FILM ver.2 (2020-03-05)"
+},
+
+{
+  "link": "https://youtu.be/WjhlsIiYE3w",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/WjhlsIiYE3w/sddefault.jpg",
+  "alt": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 S/S LOOKBOOK MAKING FILM FULL.ver (2020-03-03)",
+  "title": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 S/S LOOKBOOK MAKING FILM FULL.ver (2020-03-03)"
+},
+
+{
+  "link": "https://youtu.be/Dq0P_nHGbR8",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2020",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/Dq0P_nHGbR8/sddefault.jpg",
+  "alt": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 S/S LOOKBOOK MAKING FILM  ver.3 (2020-03-17)",
+  "title": "AKIIICLASSIC X (G)I-DLE / 아키클래식 X (여자)아이들 2020 S/S LOOKBOOK MAKING FILM  ver.3 (2020-03-17)"
+},
+
+{
+  "link": "https://youtu.be/zGiMbfcw2FM",
+  "category": "SHORTS",
+  "member": "미연",
+  "year": "2021",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/zGiMbfcw2FM/sddefault.jpg",
+  "alt": "(여자)아이들 미연이 알려주는 '아리'랑 생일 파티 이벤트 | 이벤트 트레일러 - 리그 오브 레전드 (2021-12-09)",
+  "title": "(여자)아이들 미연이 알려주는 '아리'랑 생일 파티 이벤트 | 이벤트 트레일러 - 리그 오브 레전드 (2021-12-09)"
+},
+
+{
+  "link": "https://youtu.be/-jCaSSe1lNg",
+  "category": "SHORTS",
+  "member": "아이들",
+  "year": "2021",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/-jCaSSe1lNg/sddefault.jpg",
+  "alt": "Android X Galaxy Z Flip 3 5G 튜토리얼 비디오 (2021-09-08)",
+  "title": "Android X Galaxy Z Flip 3 5G 튜토리얼 비디오 (2021-09-08)"
+},
+
+{
+  "link": "https://youtu.be/qL48R94Clk4",
+  "category": "SHORTS",
+  "member": "미연",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/qL48R94Clk4/sddefault.jpg",
+  "alt": "[ KOTRA X (G)I-DLE Miyeon ] Consumer Goods Showcase Korea ONLINE 2021(Eng sub) (2021-07-16)",
+  "title": "[ KOTRA X (G)I-DLE Miyeon ] Consumer Goods Showcase Korea ONLINE 2021(Eng sub) (2021-07-16)"
+},
+
+{
+  "link": "https://youtu.be/7Io5x0sNyO8",
+  "category": "SHORTS",
+  "member": "미연",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/7Io5x0sNyO8/sddefault.jpg",
+  "alt": "[ KOTRA X (G)I-DLE Miyeon ] 2021 CGSK K-Fashion Pavilion(Eng sub) (2021-07-16)",
+  "title": "[ KOTRA X (G)I-DLE Miyeon ] 2021 CGSK K-Fashion Pavilion(Eng sub) (2021-07-16)"
+},
+
+{
+  "link": "https://youtu.be/ld69NQsKuBU",
+  "category": "SHORTS",
+  "member": "미연",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/ld69NQsKuBU/sddefault.jpg",
+  "alt": "[ KOTRA X (G)I-DLE Miyeon ] 2021 CGSK K-Beauty Pavilion(Eng sub) (2021-07-09)",
+  "title": "[ KOTRA X (G)I-DLE Miyeon ] 2021 CGSK K-Beauty Pavilion(Eng sub) (2021-07-09)"
+},
+{
+  "link": "https://youtu.be/98ewyQ2-GUw",
+  "category": "ETC",
+  "member": "소연",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/98ewyQ2-GUw/sddefault.jpg",
+  "alt": "유연이 말하는 아이들(i-dle) 전소연 SBN 🖤",
+  "title": "유연이 말하는 아이들(i-dle) 전소연 SBN 🖤"
+},
+
+{
+  "link": "https://youtu.be/Oo9K9ago7S4",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/Oo9K9ago7S4/sddefault.jpg",
+  "alt": "👰결혼식 축무 비하인드👰with i-dle | i-dle Dance Medley🎵|  #축무 #축가 #STARSYSTEM #idle",
+  "title": "👰결혼식 축무 비하인드👰with i-dle | i-dle Dance Medley🎵|  #축무 #축가 #STARSYSTEM #idle"
+},
+
+{
+  "link": "https://youtu.be/oNxPpXiE0BI",
+  "category": "ETC",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/oNxPpXiE0BI/sddefault.jpg",
+  "alt": "\"해석 안하면 손해\" 민니 솔로 HER 반전 가사해석 | (여자)아이들 MINNIE",
+  "title": "\"해석 안하면 손해\" 민니 솔로 HER 반전 가사해석 | (여자)아이들 MINNIE"
+},
+
+{
+  "link": "https://youtu.be/7TCtgVAbMZg",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/7TCtgVAbMZg/sddefault.jpg",
+  "alt": "양보는 나이스한 도레미들, 하지만 받쓰는 낫 나이스;; 〈(여자)아이들 – 빌런 다이스♪〉#놀라운토요일 | amazingsaturday EP.349 | tvN 250118 방송",
+  "title": "양보는 나이스한 도레미들, 하지만 받쓰는 낫 나이스;; 〈(여자)아이들 – 빌런 다이스♪〉#놀라운토요일 | amazingsaturday EP.349 | tvN 250118 방송"
+},
+
+{
+  "link": "https://youtu.be/QatbI5y8jr4",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2024",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/QatbI5y8jr4/sddefault.jpg",
+  "alt": "프랭크버거 × (여자)아이들 TVC",
+  "title": "프랭크버거 × (여자)아이들 TVC"
+},
+
+{
+  "link": "https://youtu.be/SZT7bXyRq00",
+  "category": "ETC",
+  "member": "미연, 민니",
+  "year": "2023",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/SZT7bXyRq00/sddefault.jpg",
+  "alt": "(여자)아이들 미연·민니 23FW 겨울 광고 영상 공개",
+  "title": "(여자)아이들 미연·민니 23FW 겨울 광고 영상 공개"
+},
+
+{
+  "link": "https://youtu.be/uzN1na372LI",
+  "category": "ETC",
+  "member": "미연",
+  "year": "2023",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/uzN1na372LI/sddefault.jpg",
+  "alt": "[선양] 마실수록 빠져드는 선양! 미연도 빠졌다!(TVC광고_30s)",
+  "title": "[선양] 마실수록 빠져드는 선양! 미연도 빠졌다!(TVC광고_30s)"
+},
+
+{
+  "link": "https://youtu.be/swdyW8Fob44",
+  "category": "ETC",
+  "member": "미연",
+  "year": "2023",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/swdyW8Fob44/sddefault.jpg",
+  "alt": "[선양] 마실수록 빠져드는 선양! 미연도 빠졌다!(TVC광고_15s)",
+  "title": "[선양] 마실수록 빠져드는 선양! 미연도 빠졌다!(TVC광고_15s)"
+},
+
+{
+  "link": "https://youtu.be/eG4o_5OEVTw",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/eG4o_5OEVTw/sddefault.jpg",
+  "alt": "[EN] (여자)아이들 MV 속 남자는 현대무용가? | (G)I-DLE 'I DO' MV Reaction",
+  "title": "[EN] (여자)아이들 MV 속 남자는 현대무용가? | (G)I-DLE 'I DO' MV Reaction"
+},
+
+{
+  "link": "https://youtu.be/gkWhQ0d34ZI",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/gkWhQ0d34ZI/sddefault.jpg",
+  "alt": "( )아이들[(G)I-DLE], 역대급이었던 '퀸카' 활동 한 번에 몰아보기(ENG)",
+  "title": "( )아이들[(G)I-DLE], 역대급이었던 '퀸카' 활동 한 번에 몰아보기(ENG)"
+},
+
+{
+  "link": "https://youtu.be/1vBDTJcY3lQ",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/1vBDTJcY3lQ/sddefault.jpg",
+  "alt": "[탐스제로] 매일 새로움을 탐해, 너도 탐스해봐 (15초 안무편)",
+  "title": "[탐스제로] 매일 새로움을 탐해, 너도 탐스해봐 (15초 안무편)"
+},
+
+{
+  "link": "https://youtu.be/7N748bHKnII",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/7N748bHKnII/sddefault.jpg",
+  "alt": "탐스제로 x (G)I-DLE Full ver. 공개!",
+  "title": "탐스제로 x (G)I-DLE Full ver. 공개!"
+},
+
+{
+  "link": "https://youtu.be/y0ykX9IWb0g",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/y0ykX9IWb0g/sddefault.jpg",
+  "alt": "[Mnet PRIME SHOW/PRIME ARTIST] 아이돌 대표 네버버 STAYC의 (G)I-DLE 레전드 무대 리액션!?",
+  "title": "[Mnet PRIME SHOW/PRIME ARTIST] 아이돌 대표 네버버 STAYC의 (G)I-DLE 레전드 무대 리액션!?"
+},
+
+{
+  "link": "https://youtu.be/pYP7pcDnZfk",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/pYP7pcDnZfk/sddefault.jpg",
+  "alt": "[몬채널][B] EP.358 JOOHONEY 'M COUNTDOWN' poster&teaser shooting + 1st episode",
+  "title": "[몬채널][B] EP.358 JOOHONEY 'M COUNTDOWN' poster&teaser shooting + 1st episode"
+},
+
+{
+  "link": "https://youtu.be/Thm6QoLkNNs",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2023",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/Thm6QoLkNNs/sddefault.jpg",
+  "alt": "[탐스제로] 매일 새로움을 탐해, 너도 탐스해봐 (30초)",
+  "title": "[탐스제로] 매일 새로움을 탐해, 너도 탐스해봐 (30초)"
+},
+
+{
+  "link": "https://youtu.be/TInrIeI9wK8",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2022",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/TInrIeI9wK8/sddefault.jpg",
+  "alt": "[STAY:SEE👀] #56 STAYC 데뷔 2주년 파티🎉 | MUSIC BANK IN CHILE Behind",
+  "title": "[STAY:SEE👀] #56 STAYC 데뷔 2주년 파티🎉 | MUSIC BANK IN CHILE Behind"
+},
+
+{
+  "link": "https://youtu.be/7KdM2VqJBJk",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2022",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/7KdM2VqJBJk/sddefault.jpg",
+  "alt": "Proud in Seoul EP3 : พาช้อป 10 แบรนด์ดังในโซล หมดตัวที่เกาหลีมีอยู่จริง! 💸🇰🇷",
+  "title": "Proud in Seoul EP3 : พาช้อป 10 แบรนด์ดังในโซล หมดตัวที่เกาหลีมีอยู่จริง! 💸🇰🇷"
+},
+
+{
+  "link": "https://youtu.be/tuJ-6b92jaE",
+  "category": "ETC",
+  "member": "소연",
+  "year": "2022",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/tuJ-6b92jaE/sddefault.jpg",
+  "alt": "ASUS X 소연 l Unfold the Incredible, ASUS Zenbook (30s ver.)",
+  "title": "ASUS X 소연 l Unfold the Incredible, ASUS Zenbook (30s ver.)"
+},
+
+{
+  "link": "https://youtu.be/ZzZk-iOnJ48",
+  "category": "ETC",
+  "member": "민니",
+  "year": "2022",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/ZzZk-iOnJ48/sddefault.jpg",
+  "alt": "말해모해💓 예쁨 그 잡채(ღ•͈ᴗ•͈ღ) 예삐예삐 민니(MINNIE)의 올겨울 태국갈때 여기어때🌎🎵",
+  "title": "말해모해💓 예쁨 그 잡채(ღ•͈ᴗ•͈ღ) 예삐예삐 민니(MINNIE)의 올겨울 태국갈때 여기어때🌎🎵"
+},
+
+{
+  "link": "https://youtu.be/-58XaaS1Aqs",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2022",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/-58XaaS1Aqs/sddefault.jpg",
+  "alt": "KITKAT, My Gold Wish💫",
+  "title": "KITKAT, My Gold Wish💫"
+},
+
+{
+  "link": "https://youtu.be/yu_85QuSNAU",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2022",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/yu_85QuSNAU/sddefault.jpg",
+  "alt": "FIFA Official Licensed Product | Winter Film (Full)",
+  "title": "FIFA Official Licensed Product | Winter Film (Full)"
+},
+
+{
+  "link": "https://youtu.be/wqYEagKsRiw",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2022",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/wqYEagKsRiw/sddefault.jpg",
+  "alt": "FIFA Official Licensed Product Release | Brand Film (Full)",
+  "title": "FIFA Official Licensed Product Release | Brand Film (Full)"
+},
+
+{
+  "link": "https://youtu.be/n9oti9orTuM",
+  "category": "ETC",
+  "member": "소연",
+  "year": "2022",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/n9oti9orTuM/sddefault.jpg",
+  "alt": "[쉐이크쉑 X 전소연] 아따 맛있다(Ah-dda, It's delicious)(Shake Shack Korea) MV",
+  "title": "[쉐이크쉑 X 전소연] 아따 맛있다(Ah-dda, It's delicious)(Shake Shack Korea) MV"
+},
+
+{
+  "link": "https://youtu.be/IRtT2NGo1j0",
+  "category": "ETC",
+  "member": "민니",
+  "year": "2022",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/IRtT2NGo1j0/sddefault.jpg",
+  "alt": "(여자)아이들 민니랑 첫 예능 출연! 친구끼리 대한외국인 비하인드",
+  "title": "(여자)아이들 민니랑 첫 예능 출연! 친구끼리 대한외국인 비하인드"
+},
+
+{
+  "link": "https://youtu.be/jBl15ho7JOg",
+  "category": "ETC",
+  "member": "소연",
+  "year": "2022",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/jBl15ho7JOg/sddefault.jpg",
+  "alt": "전소연이 멤버들을 자랑하는 방법 / 아이들(G)I-DLE 마이백(MY BAG) 가사",
+  "title": "전소연이 멤버들을 자랑하는 방법 / 아이들(G)I-DLE 마이백(MY BAG) 가사"
+},
+
+{
+  "link": "https://youtu.be/-pMAHu9i180",
+  "category": "ETC",
+  "member": "소연",
+  "year": "2021",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/-pMAHu9i180/sddefault.jpg",
+  "alt": "세상의 기준을 불태워라! 불닭볶음면(Main casting \"(여자) 아이들 소연\")",
+  "title": "세상의 기준을 불태워라! 불닭볶음면(Main casting \"(여자) 아이들 소연\")"
+},
+
+{
+  "link": "https://youtu.be/PFzIHctDAeg",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2021",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/PFzIHctDAeg/sddefault.jpg",
+  "alt": "스마트홈은 팀삼성🤝에게 맡겨봐! 환상적인 팀삼성은 삼성 디지털프라자에서💙",
+  "title": "스마트홈은 팀삼성🤝에게 맡겨봐! 환상적인 팀삼성은 삼성 디지털프라자에서💙"
+},
+
+{
+  "link": "https://youtu.be/-jCaSSe1lNg",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2021",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/-jCaSSe1lNg/sddefault.jpg",
+  "alt": "Android X Galaxy Z Flip 3 5G 튜토리얼 비디오",
+  "title": "Android X Galaxy Z Flip 3 5G 튜토리얼 비디오"
+},
+
+{
+  "link": "https://youtu.be/YqYKiBGdfTU",
+  "category": "ETC",
+  "member": "민니",
+  "year": "2021",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/YqYKiBGdfTU/sddefault.jpg",
+  "alt": "Chinese Food Date with Elkie (CLC) & Minnie (G)I-DLE",
+  "title": "Chinese Food Date with Elkie (CLC) & Minnie (G)I-DLE"
+},
+
+{
+  "link": "https://youtu.be/E0JoAjPSrYo",
+  "category": "ETC",
+  "member": "민니",
+  "year": "2021",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/E0JoAjPSrYo/sddefault.jpg",
+  "alt": "(여자)아이들 민니랑 수다 떠는 VLOG 트라우마가 있었다고?",
+  "title": "(여자)아이들 민니랑 수다 떠는 VLOG 트라우마가 있었다고?"
+},
+
+{
+  "link": "https://youtu.be/mm9PLpV9cqs",
+  "category": "ETC",
+  "member": "민니",
+  "year": "2021",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/mm9PLpV9cqs/sddefault.jpg",
+  "alt": "(여자)아이들 민니와 휴일을 함께 했어요~ 아이돌은 뭐하고 놀까?",
+  "title": "(여자)아이들 민니와 휴일을 함께 했어요~ 아이돌은 뭐하고 놀까?"
+},
+
+{
+  "link": "https://youtu.be/uTxvzBeaa5w",
+  "category": "ETC",
+  "member": "아이들",
+  "year": "2020",
+  "month": "12",
+  "thumbnail": "https://i.ytimg.com/vi/uTxvzBeaa5w/sddefault.jpg",
+  "alt": "[티비냥] EP39-06 누구요? 핫한 걸그룹 LOL K/DA 등장에 찐으로 당황한 도레미들 증강현실?? | #놀라운토요일 181229",
+  "title": "[티비냥] EP39-06 누구요? 핫한 걸그룹 LOL K/DA 등장에 찐으로 당황한 도레미들 증강현실?? | #놀라운토요일 181229"
+},
+
+{
+  "link": "https://youtu.be/icUfcn2sTXU",
+  "category": "ETC",
+  "member": "소연",
+  "year": "2020",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/icUfcn2sTXU/sddefault.jpg",
+  "alt": "[SUB] [뮤비해석] 전소연 천재 ｜(여자)아이들 'Oh my god'｜Sub EN/ES/RU/VN/FR",
+  "title": "[SUB] [뮤비해석] 전소연 천재 ｜(여자)아이들 'Oh my god'｜Sub EN/ES/RU/VN/FR"
+}
 
 
   
