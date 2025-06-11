33b362e62e36edbe0f72f377b7fd4ed0fce87095
ssssffffff <idle52neverland@gmail.com>
2025-06-11 18:11:42 +0900

    Update cards-LIVE_STREAM.js

--- js/cards-LIVE_STREAM.js
+++ js/cards-LIVE_STREAM.js
@@ -1,5 +1,223 @@
 const liveStreamCards = [
-  // 카드 데이터를 여기에 추가하세요
+{
+  "link": "https://youtu.be/s69fJjmS1S0",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2025",
+  "month": "06",
+  "thumbnail": "https://i.ytimg.com/vi/s69fJjmS1S0/sddefault.jpg",
+  "alt": "💜아이들 데뷔 1주년 기념 V LIVE🎉💜 190502 (2025-06-08)",
+  "title": "💜아이들 데뷔 1주년 기념 V LIVE🎉💜 190502 (2025-06-08)"
+},
+
+{
+  "link": "https://youtu.be/XW1Dd4Uh2-k",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/XW1Dd4Uh2-k/sddefault.jpg",
+  "alt": "[라이브] 다섯개의 i가 모여 We가 된 i-dle 라이브 (2025-05-12)",
+  "title": "[라이브] 다섯개의 i가 모여 We가 된 i-dle 라이브 (2025-05-12)"
+},
+
+{
+  "link": "https://youtu.be/aBg10lRqyRA",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2025",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/aBg10lRqyRA/sddefault.jpg",
+  "alt": "i-dle (아이들) 7th ANNIVERSARY PARTY with NEVERLAND 🎆 (2025-05-02)",
+  "title": "i-dle (아이들) 7th ANNIVERSARY PARTY with NEVERLAND 🎆 (2025-05-02)"
+},
+
+{
+  "link": "https://youtu.be/W-T6HEHMy2Y",
+  "category": "LIVE_STREAM",
+  "member": "민니",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/W-T6HEHMy2Y/sddefault.jpg",
+  "alt": "250127 (여자)아이들 민니 인스타 라이브 | (G)I-DLE MINNIE Instagram Live (2025-01-27)",
+  "title": "250127 (여자)아이들 민니 인스타 라이브 | (G)I-DLE MINNIE Instagram Live (2025-01-27)"
+},
+
+{
+  "link": "https://youtu.be/XpipmL_reDU",
+  "category": "LIVE_STREAM",
+  "member": "우기",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/XpipmL_reDU/sddefault.jpg",
+  "alt": "250122 아이들 우기 웨이보 라이브 | (G)I-DLE Yuqi Weibo Live (2025-01-22)",
+  "title": "250122 아이들 우기 웨이보 라이브 | (G)I-DLE Yuqi Weibo Live (2025-01-22)"
+},
+
+{
+  "link": "https://youtu.be/whdJ2aH79Ak",
+  "category": "LIVE_STREAM",
+  "member": "우기",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/whdJ2aH79Ak/sddefault.jpg",
+  "alt": "250118 아이들 우기 웨이보 라이브 -  元梦之星 (Yuan Meng Star) | (G)I-DLE Yuqi Weibo Live (2025-01-20)",
+  "title": "250118 아이들 우기 웨이보 라이브 -  元梦之星 (Yuan Meng Star) | (G)I-DLE Yuqi Weibo Live (2025-01-20)"
+},
+
+{
+  "link": "https://youtu.be/QidnyVMJsxg",
+  "category": "LIVE_STREAM",
+  "member": "우기",
+  "year": "2024",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/QidnyVMJsxg/sddefault.jpg",
+  "alt": "[ENG SUB]240922 Yuqi Weibo bday live (2024-09-23)",
+  "title": "[ENG SUB]240922 Yuqi Weibo bday live (2024-09-23)"
+},
+
+{
+  "link": "https://youtu.be/DbuBqLl1IyQ",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2024",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/DbuBqLl1IyQ/sddefault.jpg",
+  "alt": "[I-LIVE] 🎉 (G)I-DLE 6TH ANNIVERSARY PARTY 💜 (2024-05-02)",
+  "title": "[I-LIVE] 🎉 (G)I-DLE 6TH ANNIVERSARY PARTY 💜 (2024-05-02)"
+},
+
+{
+  "link": "https://youtu.be/Z9OCklTW0fA",
+  "category": "LIVE_STREAM",
+  "member": "미연",
+  "year": "2024",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/Z9OCklTW0fA/sddefault.jpg",
+  "alt": "져니&며니 찐친라이브 (ENG SUB) [프로미스나인 지원/(여자)아이들 미연] (2024-04-05)",
+  "title": "져니&며니 찐친라이브 (ENG SUB) [프로미스나인 지원/(여자)아이들 미연] (2024-04-05)"
+},
+
+{
+  "link": "https://youtu.be/x3vs_eA3jZA",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2023",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/x3vs_eA3jZA/sddefault.jpg",
+  "alt": "[I-LIVE] 🎉 (G)I-DLE 5TH ANNIVERSARY PARTY with Pajamas 🎂🎉 (2023-05-01)",
+  "title": "[I-LIVE] 🎉 (G)I-DLE 5TH ANNIVERSARY PARTY with Pajamas 🎂🎉 (2023-05-01)"
+},
+
+{
+  "link": "https://youtu.be/bsXWkiZN1rk",
+  "category": "LIVE_STREAM",
+  "member": "미연, 슈화",
+  "year": "2022",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/bsXWkiZN1rk/sddefault.jpg",
+  "alt": "[LIVE] 잔망하우스 온라인 집들이 with 미연(MIYEON)&슈화(SHUHUA) | 뤂덕 네버랜드 다 모여라 | (여자)아이들X잔망루피 라이브 (2022-10-14)",
+  "title": "[LIVE] 잔망하우스 온라인 집들이 with 미연(MIYEON)&슈화(SHUHUA) | 뤂덕 네버랜드 다 모여라 | (여자)아이들X잔망루피 라이브 (2022-10-14)"
+},
+
+{
+  "link": "https://youtu.be/ka1uS3ORmxk",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2022",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/ka1uS3ORmxk/sddefault.jpg",
+  "alt": "[I-LIVE] 4랑하는 네버랜드💜 (여자)아이들 데뷔 4주년 됐어요🎂🎉 (2022-05-02)",
+  "title": "[I-LIVE] 4랑하는 네버랜드💜 (여자)아이들 데뷔 4주년 됐어요🎂🎉 (2022-05-02)"
+},
+
+{
+  "link": "https://youtu.be/m8rHmMDTRRE",
+  "category": "LIVE_STREAM",
+  "member": "소연",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/m8rHmMDTRRE/sddefault.jpg",
+  "alt": "JEON SOYEON - 'Windy Burger Grand Open!' 라이브 방송 (2021-07-06)",
+  "title": "JEON SOYEON - 'Windy Burger Grand Open!' 라이브 방송 (2021-07-06)"
+},
+
+{
+  "link": "https://youtu.be/srepY5GVRhU",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2021",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/srepY5GVRhU/sddefault.jpg",
+  "alt": "'Windy Burger Grand Open!' - 하이라이트 09 1부 레드 카펫!🥳👏🥳 (2021-07-06)",
+  "title": "'Windy Burger Grand Open!' - 하이라이트 09 1부 레드 카펫!🥳👏🥳 (2021-07-06)"
+},
+
+{
+  "link": "https://youtu.be/FBJ_STMLvS8",
+  "category": "LIVE_STREAM",
+  "member": "소연",
+  "year": "2021",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/FBJ_STMLvS8/sddefault.jpg",
+  "alt": "스페셜생방: (여자)아이들 전소연님과 함께해요~ 꺄악!!!! | 디바제시카 (2021-01-15)",
+  "title": "스페셜생방: (여자)아이들 전소연님과 함께해요~ 꺄악!!!! | 디바제시카 (2021-01-15)"
+},
+
+{
+  "link": "https://youtu.be/qTdMKpWxYeE",
+  "category": "LIVE_STREAM",
+  "member": "소연",
+  "year": "2020",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/qTdMKpWxYeE/sddefault.jpg",
+  "alt": "[I-LIVE] 소연이의 롤 게임 방송! 🎮🕹 (2020-11-11)",
+  "title": "[I-LIVE] 소연이의 롤 게임 방송! 🎮🕹 (2020-11-11)"
+},
+
+{
+  "link": "https://youtu.be/M0mFoGBRu14",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2020",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/M0mFoGBRu14/sddefault.jpg",
+  "alt": "[I-LIVE] (여자)아이들 데뷔 2주년 기념일 🎂 (2020-05-02)",
+  "title": "[I-LIVE] (여자)아이들 데뷔 2주년 기념일 🎂 (2020-05-02)"
+},
+
+{
+  "link": "https://youtu.be/T8_fTZl1TkA",
+  "category": "LIVE_STREAM",
+  "member": "소연",
+  "year": "2020",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/T8_fTZl1TkA/sddefault.jpg",
+  "alt": "[I-LIVE] 소연이의 롤 게임 방송! (2020-03-23)",
+  "title": "[I-LIVE] 소연이의 롤 게임 방송! (2020-03-23)"
+},
+
+{
+  "link": "https://youtu.be/HeHnq1tgFyg",
+  "category": "LIVE_STREAM",
+  "member": "소연",
+  "year": "2020",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/HeHnq1tgFyg/sddefault.jpg",
+  "alt": "[I-LIVE] 소연이의 롤 게임 방송! (2020-03-23)",
+  "title": "[I-LIVE] 소연이의 롤 게임 방송! (2020-03-23)"
+},
+
+{
+  "link": "https://youtu.be/sPz8C3MSdVs",
+  "category": "LIVE_STREAM",
+  "member": "아이들",
+  "year": "2020",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/sPz8C3MSdVs/sddefault.jpg",
+  "alt": "[I-LIVE] So Sweet~ Happy Valentine's Day! (2020-02-15)",
+  "title": "[I-LIVE] So Sweet~ Happy Valentine's Day! (2020-02-15)"
+}
 
 
 
