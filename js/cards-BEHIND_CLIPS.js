0e534c8ba2e19b953b27ac80418b02ace5158b8f
ssssffffff <idle52neverland@gmail.com>
2025-06-15 13:29:57 +0900

    Update cards-BEHIND_CLIPS.js

--- js/cards-BEHIND_CLIPS.js
+++ js/cards-BEHIND_CLIPS.js
@@ -5,7 +5,6 @@ const behindClipsCards = [
   "member": "아이들",
   "year": "2025",
   "month": "05",
-  "duration": "00:42:02",
   "thumbnail": "https://i.ytimg.com/vi/kfWSiV1nO9M/mqdefault.jpg",
   "alt": "i-dle(아이들).zip 📂 LATATA부터 Good Thing까지 | Show! MusicCore (2025-05-30)",
   "title": "i-dle(아이들).zip 📂 LATATA부터 Good Thing까지 | Show! MusicCore (2025-05-30)"
