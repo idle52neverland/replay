db899dc21ded8715cb00ea2a4011115b505ac775
ssssffffff <idle52neverland@gmail.com>
2025-06-15 15:02:16 +0900

    Update cards-BEHIND_CLIPS.js

--- js/cards-BEHIND_CLIPS.js
+++ js/cards-BEHIND_CLIPS.js
@@ -1,15 +1,17 @@
 const behindClipsCards = [
-{
-  "link": "https://youtu.be/kfWSiV1nO9M",
-  "category": "BEHIND_CLIPS",
-  "member": "아이들",
-  "year": "2025",
-  "month": "05",
-  "duration": "00:42:02",
-  "thumbnail": "https://i.ytimg.com/vi/kfWSiV1nO9M/mqdefault.jpg",
-  "alt": "i-dle(아이들).zip 📂 LATATA부터 Good Thing까지 | Show! MusicCore (2025-05-30)",
-  "title": "i-dle(아이들).zip 📂 LATATA부터 Good Thing까지 | Show! MusicCore (2025-05-30)"
-}
+  {
+    "link": "https://youtu.be/kfWSiV1nO9M",
+    "category": "BEHIND_CLIPS",
+    "member": "아이들",
+    "year": "2025",
+    "month": "05",
+    "duration": "00:42:02",
+    "thumbnail": "https://i.ytimg.com/vi/kfWSiV1nO9M/mqdefault.jpg",
+    "alt": "i-dle(아이들).zip 📂 LATATA부터 Good Thing까지 | Show! MusicCore (2025-05-30)",
+    "title": "i-dle(아이들).zip 📂 LATATA부터 Good Thing까지 | Show! MusicCore (2025-05-30)"
+  }
 
-  // ✅ 전역 등록
+  
+];
+
 window.behindClipsCards = behindClipsCards;
