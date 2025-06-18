e1cf60c0004f2797c32e8680a5e7678a9ed31592
ssssffffff <idle52neverland@gmail.com>
2025-06-18 23:57:19 +0900

    Update cards-SPECIAL_RELEASES.js

--- js/cards-SPECIAL_RELEASES.js
+++ js/cards-SPECIAL_RELEASES.js
@@ -1,5 +1,383 @@
 const specialReleasesCards = [
 
+{
+  "link": "https://youtu.be/UOxkGD8qRB4",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연, 미연",
+  "year": "2018",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/UOxkGD8qRB4/maxresdefault.jpg",
+  "alt": "미연,소연 - K/DA - POP/STARS (ft. Madison Beer, (G)I-DLE, Jaira Burns) | Music Video - League of Legends (2018-11-03)",
+  "title": "미연,소연 - K/DA - POP/STARS (ft. Madison Beer, (G)I-DLE, Jaira Burns) | Music Video - League of Legends (2018-11-03)",
+  "duration": "00:03:23"
+},
+{
+  "link": "https://youtu.be/_E5JtAcc3cY",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2019",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/_E5JtAcc3cY/maxresdefault.jpg",
+  "alt": "< < Feat. SOYEON > > KEY 키 'I Wanna Be (Feat. 소연 of (여자)아이들)' MV (2019-03-04)",
+  "title": "< < Feat. SOYEON > > KEY 키 'I Wanna Be (Feat. 소연 of (여자)아이들)' MV (2019-03-04)",
+  "duration": "00:03:14"
+},
+{
+  "link": "https://youtu.be/sVZpHFXcFJw",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2019",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/sVZpHFXcFJw/maxresdefault.jpg",
+  "alt": "소연 - True Damage - GIANTS (ft. Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose) | League of Legends (2019-11-10)",
+  "title": "소연 - True Damage - GIANTS (ft. Becky G, Keke Palmer, SOYEON, DUCKWRTH, Thutmose) | League of Legends (2019-11-10)",
+  "duration": "00:03:19"
+},
+{
+  "link": "https://youtu.be/4VmLuWDkHYQ",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2020",
+  "month": "07",
+  "thumbnail": "https://i.ytimg.com/vi/4VmLuWDkHYQ/maxresdefault.jpg",
+  "alt": "< < Feat. SOYEON > > HYO 효연 ‘DESSERT (Feat. Loopy, SOYEON ((G)I-DLE))’ MV (2020-07-22)",
+  "title": "< < Feat. SOYEON > >‘DESSERT (Feat. Loopy, SOYEON ((G)I-DLE))’ MV (2020-07-22)",
+  "duration": "00:03:23"
+},
+{
+  "link": "https://youtu.be/Lx4ka2qBY_A",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2021",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/Lx4ka2qBY_A/maxresdefault.jpg",
+  "alt": "민니 - F.HERO x URBOYTJ Ft. MINNIE ((G)I-DLE) - MONEY HONEY (Prod. By URBOYTJ) [Official MV] (2021-09-30)",
+  "title": "민니 - F.HERO x URBOYTJ Ft. MINNIE ((G)I-DLE) - MONEY HONEY (Prod. By URBOYTJ) [Official MV] (2021-09-30)",
+  "duration": "00:04:16"
+},
+{
+  "link": "https://youtu.be/bKLTsAy9ig4",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2023",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/bKLTsAy9ig4/maxresdefault.jpg",
+  "alt": "[MV] SOYEON ((G)I-DLE) X MOONO (소연 ((여자)아이들) X 무너) _ I'm OK(아무너케) (2023-08-23)",
+  "title": "[MV] SOYEON ((G)I-DLE) X MOONO (소연 ((여자)아이들) X 무너) _ I'm OK(아무너케) (2023-08-23)",
+  "duration": "00:03:51"
+},
+{
+  "link": "https://youtu.be/NL-uanxNldk",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2023",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/NL-uanxNldk/maxresdefault.jpg",
+  "alt": "< < Feat. MINNIE > > 로꼬 (Loco) - 'NOT OK (Feat. 민니 ((여자)아이들))' Official Music Video [ENG/CHN] (2023-10-17)",
+  "title": "< <Feat. MINNIE > > 로꼬 (Loco) - 'NOT OK (Feat. 민니 ((여자)아이들))' Official Music Video [ENG/CHN] (2023-10-17)",
+  "duration": "00:03:33"
+},
+{
+  "link": "https://youtu.be/N68rwFjyinI",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2023",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/N68rwFjyinI/maxresdefault.jpg",
+  "alt": "[MV] SOYEON of (G)I-DLE X WINTER of aespa X LIZ of IVE 'NOBODY' (2023-11-16)",
+  "title": "[MV] SOYEON of (G)I-DLE X WINTER of aespa X LIZ of IVE 'NOBODY' (2023-11-16)",
+  "duration": "00:02:49"
+},
+{
+  "link": "https://youtu.be/8OfUx3yY_B8",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2024",
+  "month": "09",
+  "thumbnail": "https://i.ytimg.com/vi/8OfUx3yY_B8/maxresdefault.jpg",
+  "alt": "MIYEON(미연) LOVIN’ MY STAY MV | Experience Macao Limited Edition (2024-09-17)",
+  "title": "MIYEON(미연) LOVIN’ MY STAY MV | Experience Macao Limited Edition (2024-09-17)",
+  "duration": "00:02:04"
+},
+
+{
+  "link": "https://youtu.be/ZlpxcCiCCP4",
+  "category": "SPECIAL_RELEASES",
+  "member": "슈화",
+  "year": "2025",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/ZlpxcCiCCP4/maxresdefault.jpg",
+  "alt": "周興哲Eric Chou《幾乎是愛情 Almost》Official Music Video (特邀主演:舒華 Shuhua 슈화) (2025-01-22)",
+  "title": "周興哲Eric Chou《幾乎是愛情 Almost》Official Music Video (特邀主演:舒華 Shuhua 슈화) (2025-01-22)",
+  "duration": "00:04:38"
+},
+  {
+  "link": "https://youtu.be/nqBnrY6GrtY",
+  "category": "SPECIAL_RELEASES",
+  "member": "우기",
+  "year": "2025",
+  "month": "06",
+  "duration": "00:03:27",
+  "thumbnail": "https://i.ytimg.com/vi/nqBnrY6GrtY/maxresdefault.jpg",
+  "alt": "< < Prod.YUQI > > NOWZ (나우즈) '자유롭게 날아 (Feat. 우기 (YUQI))' Official Music Video (2025-06-17)",
+  "title": "< < Prod.YUQI > > NOWZ (나우즈) '자유롭게 날아 (Feat. 우기 (YUQI))' Official Music Video (2025-06-17)"
+},
+  {
+  "link": "https://youtu.be/uWT79Qq52sU",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2025",
+  "month": "06",
+  "duration": "00:02:58",
+  "thumbnail": "https://i.ytimg.com/vi/uWT79Qq52sU/mqdefault.jpg",
+  "alt": "< < Prod.SOYEON > > Baby DONT Cry - '지금을 놓치면 분명 너 후회할 거야' MV (2025-06-15)",
+  "title": "< < Prod.SOYEON > > Baby DONT Cry - '지금을 놓치면 분명 너 후회할 거야' MV (2025-06-15)"
+},
+  {
+  "link": "https://youtu.be/AlirzLFEHUI",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2024",
+  "month": "09",
+  "duration": "00:03:16",
+  "thumbnail": "https://i.ytimg.com/vi/AlirzLFEHUI/mqdefault.jpg",
+  "alt": "< < Prod.SOYEON > > QWER '내 이름 맑음' Official MV (2024-09-23)",
+  "title": "< < Prod.SOYEON > > QWER '내 이름 맑음' Official MV (2024-09-23)"
+},
+  {
+  "link": "https://youtu.be/en4BDQgflG0",
+  "category": "SPECIAL_RELEASES",
+  "member": "소연",
+  "year": "2020",
+  "month": "09",
+  "duration": "00:03:06",
+  "thumbnail": "https://i.ytimg.com/vi/en4BDQgflG0/mqdefault.jpg",
+  "alt": "< < Written by SOYEON > > (김남주) 'Bird' MV (2020-09-07)",
+  "title": "< < Written by SOYEON > > (김남주) 'Bird' MV (2020-09-07)"
+},
+
+  {
+  "link": "https://youtu.be/1XIi9ofX2kE",
+  "category": "SPECIAL_RELEASES",
+  "member": "우기",
+  "year": "2023",
+  "month": "06",
+  "duration": "00:03:49",
+  "thumbnail": "https://i.ytimg.com/vi/1XIi9ofX2kE/mqdefault.jpg",
+  "alt": "< < Feat. YUQI > > YENA (최예나) - Hate Rodrigo (Feat. 우기 ((여자)아이들)) MV (2023-06-30)",
+  "title": "< < Feat. YUQI > > YENA (최예나) - Hate Rodrigo (Feat. 우기 ((여자)아이들)) MV (2023-06-30)"
+},
+
+  {
+  "link": "https://youtu.be/eA2QBBM8s8s",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2021",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/eA2QBBM8s8s/sddefault.jpg",
+  "alt": "[어른 연습생 OST Part 2] 미연 ((여자)아이들) - Imagine Love (2021-11-21)",
+  "title": "[어른 연습생 OST Part 2] 미연 ((여자)아이들) - Imagine Love (2021-11-21)",
+  "duration": "00:03:09"
+},
+{
+  "link": "https://youtu.be/HxROxHdBQ2g",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/HxROxHdBQ2g/maxresdefault.jpg",
+  "alt": "미연(MIYEON), Jike Junyi - 'Glow Up' SPECIAL CLIP (2025-04-17)",
+  "title": "미연(MIYEON), Jike Junyi - 'Glow Up' SPECIAL CLIP (2025-04-17)",
+  "duration": "00:03:27"
+},
+{
+  "link": "https://youtu.be/_lQxaYmt0DQ",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2024",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/_lQxaYmt0DQ/maxresdefault.jpg",
+  "alt": "[선재 업고 튀어 OST Part 3] 민니 ((여자)아이들) (MINNIE) - 꿈결같아서 (Like A Dream) MV (2024-04-17)",
+  "title": "[선재 업고 튀어 OST Part 3] 민니 ((여자)아이들) (MINNIE) - 꿈결같아서 (Like A Dream) MV (2024-04-17)",
+  "duration": "00:04:10"
+},
+{
+  "link": "https://youtu.be/3Ccu3CjsPZo",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2024",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/3Ccu3CjsPZo/maxresdefault.jpg",
+  "alt": "[making] 미연 ((여자)아이들) - 기도 (2024-11-13)",
+  "title": "[making] 미연 ((여자)아이들) - 기도 (2024-11-13)",
+  "duration": "00:04:36"
+},
+{
+  "link": "https://youtu.be/pPLX4Y37dD0",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2023",
+  "month": "01",
+  "thumbnail": "https://i.ytimg.com/vi/pPLX4Y37dD0/maxresdefault.jpg",
+  "alt": "메이킹 MV | 민니 ((여자)아이들) MINNIE ((G)I-DLE) - In The Novel | 모든 게 착각이었다 OST Part 2 (2023-01-26)",
+  "title": "메이킹 MV | 민니 ((여자)아이들) MINNIE ((G)I-DLE) - In The Novel | 모든 게 착각이었다 OST Part 2 (2023-01-26)",
+  "duration": "00:03:02"
+},
+{
+  "link": "https://youtu.be/FaSYRHyd-2E",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2023",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/FaSYRHyd-2E/maxresdefault.jpg",
+  "alt": "[에버소울] 재클린 OST 📀 「무덤덤(Numb)」 song by 미연 (2023-02-01)",
+  "title": "[에버소울] 재클린 OST 📀 「무덤덤(Numb)」 song by 미연 (2023-02-01)",
+  "duration": "00:03:35"
+},
+{
+  "link": "https://youtu.be/YlEahTLcL_Y",
+  "category": "SPECIAL_RELEASES",
+  "member": "우기",
+  "year": "2023",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/YlEahTLcL_Y/maxresdefault.jpg",
+  "alt": "(여자)아이들 (G)I-DLE 우기(YUQI) X 하이퍼타임(HypeerTime) 'Not Cinderella' Live Clip - 신데렐라는 내가 아니었다 OST (2023-02-23)",
+  "title": "(여자)아이들 (G)I-DLE 우기(YUQI) X 하이퍼타임(HypeerTime) 'Not Cinderella' Live Clip - 신데렐라는 내가 아니었다 OST (2023-02-23)",
+  "duration": "00:04:02"
+},
+{
+  "link": "https://youtu.be/O2sNRgiTidc",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2023",
+  "month": "08",
+  "thumbnail": "https://i.ytimg.com/vi/O2sNRgiTidc/maxresdefault.jpg",
+  "alt": "[연인] OST, 미연((여자)아이들) '달빛에 그려지는' Full Ver., MBC 230819 방송 (2023-08-21)",
+  "title": "[연인] OST, 미연((여자)아이들) '달빛에 그려지는' Full Ver., MBC 230819 방송 (2023-08-21)",
+  "duration": "00:04:01"
+},
+{
+  "link": "https://youtu.be/C5VwU4rdeHU",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2023",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/C5VwU4rdeHU/maxresdefault.jpg",
+  "alt": "[오늘도 사랑스럽개 OST] 민니((여자)아이들) MINNIE((G)I-DLE) '타이밍(Timing)' Special CLIP : 오늘도 사랑스럽개 OST Part. 2 (2023-11-15)",
+  "title": "[오늘도 사랑스럽개 OST] 민니((여자)아이들) MINNIE((G)I-DLE) '타이밍(Timing)' Special CLIP : 오늘도 사랑스럽개 OST Part. 2 (2023-11-15)",
+  "duration": "00:02:56"
+},
+{
+  "link": "https://youtu.be/wFikxHi9WO0",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2024",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/wFikxHi9WO0/maxresdefault.jpg",
+  "alt": "[연애남매] 미연 ((여자)아이들) - You and I (연애남매 OST Part.2) | Making🎙️ | My Sibling's Romance OST | GOGOSING (2024-03-22)",
+  "title": "[연애남매] 미연 ((여자)아이들) - You and I (연애남매 OST Part.2) | Making🎙️ | My Sibling's Romance OST | GOGOSING (2024-03-22)",
+  "duration": "00:02:26"
+},
+{
+  "link": "https://youtu.be/eOFfOLZOp0M",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/eOFfOLZOp0M/maxresdefault.jpg",
+  "alt": "[MaumC Record] 민니 ((여자)아이들) | 숨 | Recording Behind | 녹음 비하인드 (2025-04-26)",
+  "title": "[MaumC Record] 민니 ((여자)아이들) | 숨 | Recording Behind | 녹음 비하인드 (2025-04-26)",
+  "duration": "00:12:57"
+},
+{
+  "link": "https://youtu.be/UUh3rlx3lSw",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2022",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/UUh3rlx3lSw/maxresdefault.jpg",
+  "alt": "[Making] 미연 보컬과 함께하니 애틋함이 두배로✌ 'Someday' [꽃 피면 달 생각하고 OST Part.8 (Moonshine OST Part.8)] (2022-02-08)",
+  "title": "[Making] 미연 보컬과 함께하니 애틋함이 두배로✌ 'Someday' [꽃 피면 달 생각하고 OST Part.8 (Moonshine OST Part.8)] (2022-02-08)",
+  "duration": "00:01:38"
+},
+{
+  "link": "https://youtu.be/YVjugI2zlk4",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2020",
+  "month": "10",
+  "thumbnail": "https://i.ytimg.com/vi/YVjugI2zlk4/maxresdefault.jpg",
+  "alt": "[Making] 민니((여자)아이들) - Getaway (MBN 나의 위험한 아내 OST) 메이킹 필름 (2020-10-14)",
+  "title": "[Making] 민니((여자)아이들) - Getaway (MBN 나의 위험한 아내 OST) 메이킹 필름 (2020-10-14)",
+  "duration": "00:01:22"
+},
+  {
+  "link": "https://youtu.be/8V2SpdoRxTk",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2025",
+  "month": "02",
+  "thumbnail": "https://i.ytimg.com/vi/8V2SpdoRxTk/maxresdefault.jpg",
+  "alt": "[MV] MIYEON ((G)I-DLE))(미연 ((여자)아이들)) _ Red Moon(적월) (2025-02-12)",
+  "title": "[MV] MIYEON ((G)I-DLE))(미연 ((여자)아이들)) _ Red Moon(적월) (2025-02-12)",
+  "duration": "00:04:17"
+},
+{
+  "link": "https://youtu.be/3Ccu3CjsPZo",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2024",
+  "month": "11",
+  "thumbnail": "https://i.ytimg.com/vi/3Ccu3CjsPZo/maxresdefault.jpg",
+  "alt": "[making] 미연 ((여자)아이들) - 기도 (2024-11-13)",
+  "title": "[making] 미연 ((여자)아이들) - 기도 (2024-11-13)",
+  "duration": "00:04:36"
+},
+{
+  "link": "https://youtu.be/OAISVzre8QQ",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2025",
+  "month": "03",
+  "thumbnail": "https://i.ytimg.com/vi/OAISVzre8QQ/maxresdefault.jpg",
+  "alt": "[MV] MINNIE(민니) ((G)I-DLE ((여자)아이들)) - Draw the Moon(달이 그려지다) (feat. MIYAVI) (2025-03-21)",
+  "title": "[MV] MINNIE(민니) ((G)I-DLE ((여자)아이들)) - Draw the Moon(달이 그려지다) (feat. MIYAVI) (2025-03-21)",
+  "duration": "00:04:13"
+},
+{
+  "link": "https://youtu.be/_lQxaYmt0DQ",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2024",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/_lQxaYmt0DQ/maxresdefault.jpg",
+  "alt": "[선재 업고 튀어 OST Part 3] 민니 ((여자)아이들) (MINNIE) - 꿈결같아서 (Like A Dream) MV (2024-04-17)",
+  "title": "[선재 업고 튀어 OST Part 3] 민니 ((여자)아이들) (MINNIE) - 꿈결같아서 (Like A Dream) MV (2024-04-17)",
+  "duration": "00:04:10"
+},
+{
+  "link": "https://youtu.be/HxROxHdBQ2g",
+  "category": "SPECIAL_RELEASES",
+  "member": "미연",
+  "year": "2025",
+  "month": "04",
+  "thumbnail": "https://i.ytimg.com/vi/HxROxHdBQ2g/maxresdefault.jpg",
+  "alt": "미연(MIYEON), Jike Junyi - 'Glow Up' SPECIAL CLIP (2025-04-17)",
+  "title": "미연(MIYEON), Jike Junyi - 'Glow Up' SPECIAL CLIP (2025-04-17)",
+  "duration": "00:03:27"
+},
+{
+  "link": "https://youtu.be/jrt4iWhofPA",
+  "category": "SPECIAL_RELEASES",
+  "member": "민니",
+  "year": "2023",
+  "month": "03",
+  "duration": "00:03:16",
+  "thumbnail": "https://i.ytimg.com/vi/jrt4iWhofPA/mqdefault.jpg",
+  "alt": "Anne-Marie, 민니 ((여자)아이들) - Expectations (2023-03-09)",
+  "title": "Anne-Marie, 민니 ((여자)아이들) - Expectations (2023-03-09)"
+}
+
 
 
 // ✅ 전역 등록
