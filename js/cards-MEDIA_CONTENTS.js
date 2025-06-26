9b6ffa035b6dffccfb59f4a2579a149919dde945
ssssffffff <idle52neverland@gmail.com>
2025-06-27 06:03:16 +0900

    Update cards-MEDIA_CONTENTS.js

--- js/cards-MEDIA_CONTENTS.js
+++ js/cards-MEDIA_CONTENTS.js
@@ -36,6 +36,18 @@ const mediaContentsCards = [
 },
 
 {
+  "link": "https://youtu.be/Yeed17jb814",
+  "category": "MEDIA_CONTENTS",
+  "member": "아이들",
+  "year": "2018",
+  "month": "05",
+  "thumbnail": "https://i.ytimg.com/vi/Yeed17jb814/sddefault.jpg",
+  "alt": "[Celuv.TV/셀럽티비] '(여자)아이들' 데뷔 쇼케이스 (2018-05-03)",
+  "title": "[Celuv.TV/셀럽티비] '(여자)아이들' 데뷔 쇼케이스 (2018-05-03)",
+  "duration": "01:08:19"
+},
+  
+{
   "link": "https://youtu.be/viqFO-U77x8",
   "category": "MEDIA_CONTENTS",
   "member": "우기",
