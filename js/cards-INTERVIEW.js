e8abd096427c219bf5d1e7053b01b694d1b0172f
ssssffffff <idle52neverland@gmail.com>
2025-07-06 20:59:15 +0900

    Update cards-INTERVIEW.js

--- js/cards-INTERVIEW.js
+++ js/cards-INTERVIEW.js
@@ -1791,6 +1791,7 @@ const interviewCards = [
 }
 
 
+  
 ];
 const parseDate = (d) => new Date(d.includes("T") ? d : `${d}T00:00:00`);
 liveStreamCards.sort((a, b) => parseDate(b.date) - parseDate(a.date));
