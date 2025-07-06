9f9fea862d254939cd607c67ab3d9d15505f3698
ssssffffff <idle52neverland@gmail.com>
2025-07-06 21:45:44 +0900

    Update cards-RADIO_PODCAST.js

--- js/cards-RADIO_PODCAST.js
+++ js/cards-RADIO_PODCAST.js
@@ -1432,7 +1432,7 @@ const radioPodcastCards = [
 
   ];
 const parseDate = (d) => new Date(d.includes("T") ? d : `${d}T00:00:00`);
-liveStreamCards.sort((a, b) => parseDate(b.date) - parseDate(a.date));
+radioPodcastCards.sort((a, b) => parseDate(b.date) - parseDate(a.date));
 
 window.radioPodcastCards = radioPodcastCards;
   
