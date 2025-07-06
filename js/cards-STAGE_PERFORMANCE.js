821f00d9ee2f7fc52f6b08b4dff71db4431b9fd6
ssssffffff <idle52neverland@gmail.com>
2025-07-06 21:49:10 +0900

    Update cards-STAGE_PERFORMANCE.js

--- js/cards-STAGE_PERFORMANCE.js
+++ js/cards-STAGE_PERFORMANCE.js
@@ -10108,6 +10108,6 @@ const stagePerformanceCards = [
 
 ];
 const parseDate = (d) => new Date(d.includes("T") ? d : `${d}T00:00:00`);
-liveStreamCards.sort((a, b) => parseDate(b.date) - parseDate(a.date));
+stagePerformanceCards.sort((a, b) => parseDate(b.date) - parseDate(a.date));
 
 window.stagePerformanceCards = stagePerformanceCards;
