da5980721e62b1dcd35295794d546641529d0ef8
ssssffffff <idle52neverland@gmail.com>
2025-07-06 21:03:31 +0900

    Update cards-MV_SPECIAL_CLIP.js

--- js/cards-MV_SPECIAL_CLIP.js
+++ js/cards-MV_SPECIAL_CLIP.js
@@ -1211,5 +1211,7 @@ const mvSpecialClipCards = [
 }
 
 ];
+const parseDate = (d) => new Date(d.includes("T") ? d : `${d}T00:00:00`);
+liveStreamCards.sort((a, b) => parseDate(b.date) - parseDate(a.date));
 
 window.mvSpecialClipCards = mvSpecialClipCards;
