e812bcf7808a7ff332c5b455a9aed238512f7b57
ssssffffff <idle52neverland@gmail.com>
2025-07-15 05:48:49 +0900

    Add files via upload

---
+++ js/cards-Festival_Stage.js
@@ -0,0 +1,18 @@
+const festivalStageCards = [
+{
+  "link": "https://youtu.be/dU4JD7bYVyg",
+  "category": "Festival_Stage",
+  "member": "아이들",
+  "date": "2018-09-01",
+  "duration": "00:03:28",
+  "thumbnail": "https://i.ytimg.com/vi/dU4JD7bYVyg/maxresdefault.jpg",
+  "alt": "[인천 한류 관광 콘서트/INK CONCERT] (여자)아이들 - 한 (一)(Alone) 직캠 (2018-09-01)",
+  "title": "[인천 한류 관광 콘서트/INK CONCERT] (여자)아이들 - 한 (一)(Alone) 직캠 (2018-09-01)"
+}
+
+
+
+
+   ];
+
+window.festivalStageCards = festivalStageCards;
